e06b1f228b8dff01898666c42d866c1ba6f2fe64
Charl P. Botha <cpbotha@users.noreply.github.com>
2002-02-13 19:33:57 +0000

    Changed some module names.

---
+++ modules/vtk_vol16_rdr.py
@@ -0,0 +1,11 @@
+class vtk_volume16_reader:
+    def __init__(self):
+	# initialise vtkVolume16Reader
+	self.reader = vtkVolume16Reader()
+	
+    def __del__(self):
+	print "vtk_volume16_reader.__del__()"
+
+    def get_output(self):
+	return self.reader.GetOutput()
+    
