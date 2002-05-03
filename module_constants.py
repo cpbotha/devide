f689df47163a194ec37e9bcdb0ce6efcbae57005
Charl P. Botha <cpbotha@users.noreply.github.com>
2002-05-03 16:00:14 +0000

    Initial import with VTK type constants.

---
+++ module_constants.py
@@ -0,0 +1,17 @@
+# $Id: module_constants.py,v 1.1 2002/05/03 16:00:14 cpbotha Exp $
+
+vtk_types = {
+'VTK_VOID' : 0,
+'VTK_BIT' : 1,
+'VTK_CHAR' : 2,
+'VTK_UNSIGNED_CHAR' : 3,
+'VTK_SHORT' : 4,
+'VTK_UNSIGNED_SHORT' : 5,
+'VTK_INT' : 6,
+'VTK_UNSIGNED_INT' : 7,
+'VTK_LONG' : 8,
+'VTK_UNSIGNED_LONG' : 9,
+'VTK_FLOAT' : 10,
+'VTK_DOUBLE' : 11,
+'VTK_ID_TYPE' : 12,
+}
