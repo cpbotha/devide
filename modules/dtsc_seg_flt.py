0ddb462388cb3260a0bf39f15bc14b520ac38d1a
Charl P. Botha <cpbotha@users.noreply.github.com>
2002-09-10 15:54:58 +0000

    Small changes.

--- modules/dtsc_seg_flt.py
+++ modules/dtsc_seg_flt.py
@@ -1,5 +1,5 @@
 # dtsc_seg_flt.py copyright (c) 2002 Charl P. Botha <cpbotha@ieee.org>
-# $Id: dtsc_seg_flt.py,v 1.1 2002/09/09 15:40:34 cpbotha Exp $
+# $Id: dtsc_seg_flt.py,v 1.2 2002/09/10 15:54:58 cpbotha Exp $
 # double-threshold seed connectivity segmentation filter
 
 from gen_utils import log_error
@@ -10,11 +10,31 @@ from wxPython.wx import *
 from vtk.wx.wxVTKRenderWindowInteractor import wxVTKRenderWindowInteractor
 
 class dtsc_seg_flt(module_base,
-                       module_mixin_vtk_pipeline_config):
+                   module_mixin_vtk_pipeline_config):
+
+    """Double threshold seed connectivity segmentor.
+
+    This will perform a upper/lower threshold on the input data, thus
+    binarising it and then perform a seed-initialised connectivity
+    analysis.
+    """
 
     def __init__(self, module_manager):
-        
+        # call parent __init__
         module_base.__init__(self, module_manager)
+
+        # setup VTK pipeline
+        self._threshold = vtk.vtkImageThreshold()
+        self._seedconnect = vtk.vtkImageSeedConnectivity()
+        self._seedconnect.SetInput(self._threshold.GetOutput())
+
+        # create gui
+        pw = self._module_manager.get_module_view_parent_view_parent_window()
+        self._view_frame = self._create_view_frame(pw)
+
+    #################################################################
+    # module API methods
+    #################################################################
 
     def close(self):
         pass
@@ -33,5 +53,13 @@ class dtsc_seg_flt(module_base,
         pass
 
     def view(self):
+        pass
+
+    #################################################################
+    # utility methods
+    #################################################################
+
+    def _create_view_frame(self, parent_window):
+
         pass
 
