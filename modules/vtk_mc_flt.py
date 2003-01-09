82f9e6bff2ab13e2317fd6df28a1c3307a0e8a83
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-01-09 16:35:18 +0000

    Changes with regards to UI generation.

--- modules/vtk_mc_flt.py
+++ modules/vtk_mc_flt.py
@@ -1,5 +1,5 @@
 # vtk_mc_flt.py copyright (c) 2002 Charl P. Botha <cpbotha@ieee.org>
-# $Id: vtk_mc_flt.py,v 1.13 2003/01/08 16:16:57 cpbotha Exp $
+# $Id: vtk_mc_flt.py,v 1.14 2003/01/09 16:35:17 cpbotha Exp $
 # vtk marching cubes filter
 
 from gen_utils import log_error
@@ -7,7 +7,7 @@ from module_base import module_base, module_mixin_vtk_pipeline_config
 import module_utils
 import vtk
 from wxPython.wx import *
-from vtk.wx.wxVTKRenderWindowInteractor import wxVTKRenderWindowInteractor
+
 
 class vtk_mc_flt(module_base):
     
@@ -21,7 +21,10 @@ class vtk_mc_flt(module_base):
                                   mm.vtk_progress_cb(s.mc))
 
         # setup our gui
-        self._create_view_frame()
+        self._view_frame = None
+        parent_window = self._module_manager.get_module_view_parent_window()
+        self._create_view_frame(parent)
+
 
     #################################################################
     # module API methods
@@ -34,7 +37,8 @@ class vtk_mc_flt(module_base):
         # take care of that view_window
         self._view_frame.Destroy()
 	# check that self.mc isn't deleted yet
-        del self.mc
+        if hasattr(self, 'mc'):
+            del self.mc
 
     def execute_module(self):
         self.mc.Update()
@@ -54,54 +58,26 @@ class vtk_mc_flt(module_base):
 
     def sync_config(self):
         # for now we act as if we have one isovalue
-        self.isovalue.set(self.mc.GetValue(0))
+        # self.isovalue.set(self.mc.GetValue(0))
 
     def view(self, parent_window=None):
         self.sync_config()
-        self.config_window.deiconify()
+        self._view_frame.Show(true)        
         
     #################################################################
     # utility methods
     #################################################################
     
-    def create_view_window(self, parent_window=None):
+    def _create_view_frame(self, parent_window=None):
         """Create configuration/view window.
 
         This method sets up the config window and immediately hides it.  When
         the user wants to view/config, the window is simply de-iconised.
         """
-	self.config_window = Tkinter.Toplevel(parent_window)
-	self.config_window.title("vtk_mc_flt.py configuration")
-	self.config_window.protocol ("WM_DELETE_WINDOW",
-                                     self.config_window.withdraw)
-
-
-        iso_entry = Pmw.EntryField(self.config_window,
-                                   labelpos = 'w',
-                                   value = '55.5',
-                                   label_text = 'Isovalue:',
-                                   validate = {'validator' : 'real'},
-                                   entry_textvariable=self.isovalue)
-        iso_entry.grid(row=0,column=0)
-        #iso_entry.pack(side=TOP)
+        import resources.python.vtk_mc_flt_view
+        reload(resources.python.vtk_mc_flt_view)
         
-	
-	# button box
-	box = Pmw.ButtonBox(self.config_window)
-	box.add('vtkMarchingCubes', command=lambda self=self,
-                pw=parent_window: self.configure_vtk_object(self.mc, pw))
-	box.add('pipeline', text='Pipeline', command=lambda self=self,
-                pw=parent_window, vtk_objs=(self.mc):
-                self.browse_vtk_pipeline(vtk_objs, pw))
-	box.grid(row=1, column=0, columnspan=1, sticky=W+E)
-        #box.pack(side=TOP)
-        box.alignbuttons()
+        self._view_frame = vtk_mc_flt_view.vtk_mc_flt_view_frame(parent_window)
 
-        self.add_CSAEO_box(self.config_window, 2, 1)
-        
-        self.config_window.withdraw()
-
-	
-    
-
-
+        EVT_CLOSE(self._view_frame, lambda e, s=self: s._view_frame.Show(false))
+        module_utils.bind_CSAEO2(self, self._view_frame)
