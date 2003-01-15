8593acc22ec7fac49dd99b7d78dd84eb93d8ef85
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-01-15 17:45:54 +0000

    Updated to vtkdscas.

--- installer/hooks/hook-module_manager.py
+++ installer/hooks/hook-module_manager.py
@@ -4,6 +4,6 @@ sys.path.insert(0, '/home/cpbotha/work/code/dscas3/')
 import modules
 
 hiddenimports = ['module_base', 'module_constants', 'module_utils', \
-                 'vtkcpbothapython'] + modules.module_list
+                 'vtkdscas'] + modules.module_list
 
 
