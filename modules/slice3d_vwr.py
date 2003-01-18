44f118ac98926276eb97e1b0d81dcd58d8b69beb
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-01-18 11:05:46 +0000

    Fix to get scrollbars to adjust after addition.

--- modules/slice3d_vwr.py
+++ modules/slice3d_vwr.py
@@ -1,5 +1,5 @@
 # slice3d_vwr.py copyright (c) 2002 Charl P. Botha <cpbotha@ieee.org>
-# $Id: slice3d_vwr.py,v 1.2 2003/01/18 01:19:06 cpbotha Exp $
+# $Id: slice3d_vwr.py,v 1.3 2003/01/18 11:05:46 cpbotha Exp $
 # next-generation of the slicing and dicing dscas3 module
 
 # TODO:
@@ -539,6 +539,7 @@ class slice3d_vwr(module_base,
 
         self._view_frame.spointsGrid.AppendRows()
         row = self._view_frame.spointsGrid.GetNumberRows() - 1
+	self._view_frame.spointsGrid.AdjustScrollbars()
         self._view_frame.spointsGrid.SetCellValue(row, 0, pos_str)
         self._view_frame.spointsGrid.SetCellValue(row, 1, str(cursor[3]))
         
