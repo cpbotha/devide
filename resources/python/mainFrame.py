b3c7fba75f636aab49ea9484bd9557888b43076c
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-01-26 02:17:41 +0000

    Inserted panel as background, else colours are bad.

--- resources/python/mainFrame.py
+++ resources/python/mainFrame.py
@@ -8,8 +8,8 @@ class mainFrame(wxFrame):
         # begin wxGlade: mainFrame.__init__
         kwds["style"] = wxDEFAULT_FRAME_STYLE
         wxFrame.__init__(self, *args, **kwds)
-        self.progressText = wxStaticText(self, -1, "This is quite a long progress message so that eek.")
-        self.progressGauge = wxGauge(self, -1, 100)
+        self.panel_1 = wxPanel(self, -1)
+        self.frame_1_statusbar = self.CreateStatusBar(1)
         
         # Menu Bar
         self.frame_1_menubar = wxMenuBar()
@@ -29,7 +29,8 @@ class mainFrame(wxFrame):
         wxglade_tmp_menu.Append(self.helpAboutId , "&About", "Get information about DSCAS3")
         self.frame_1_menubar.Append(wxglade_tmp_menu, "&Help")
         # Menu Bar end
-        self.frame_1_statusbar = self.CreateStatusBar(1)
+        self.progressText = wxStaticText(self.panel_1, -1, "This is quite a long progress message so that eek.")
+        self.progressGauge = wxGauge(self.panel_1, -1, 100)
 
         self.__set_properties()
         self.__do_layout()
@@ -38,23 +39,26 @@ class mainFrame(wxFrame):
     def __set_properties(self):
         # begin wxGlade: mainFrame.__set_properties
         self.SetTitle("DSCAS3 main window")
-        self.progressText.SetFont(wxFont(14, wxDEFAULT, wxNORMAL, wxNORMAL, 0, ""))
-        self.progressGauge.SetBackgroundColour(wxColour(50, 153, 204))
-        self.progressGauge.SetForegroundColour(wxColour(0, 0, 0))
         self.frame_1_statusbar.SetStatusWidths([-1])
         # statusbar fields
         frame_1_statusbar_fields = ["Welcome to DSCAS3"]
         for i in range(len(frame_1_statusbar_fields)):
             self.frame_1_statusbar.SetStatusText(frame_1_statusbar_fields[i], i)
+        self.progressText.SetFont(wxFont(14, wxDEFAULT, wxNORMAL, wxNORMAL, 0, ""))
+        self.progressGauge.SetBackgroundColour(wxColour(50, 153, 204))
         # end wxGlade
 
     def __do_layout(self):
         # begin wxGlade: mainFrame.__do_layout
         sizer_1 = wxBoxSizer(wxVERTICAL)
-        sizer_2 = wxBoxSizer(wxHORIZONTAL)
-        sizer_1.Add(self.progressText, 0, wxLEFT|wxRIGHT|wxTOP, 5)
-        sizer_2.Add(self.progressGauge, 1, wxLEFT|wxRIGHT|wxBOTTOM, 5)
-        sizer_1.Add(sizer_2, 0, wxEXPAND, 0)
+        sizer_3 = wxBoxSizer(wxVERTICAL)
+        sizer_3.Add(self.progressText, 0, wxLEFT|wxRIGHT|wxTOP, 5)
+        sizer_3.Add(self.progressGauge, 0, wxLEFT|wxRIGHT|wxBOTTOM|wxEXPAND, 5)
+        self.panel_1.SetAutoLayout(1)
+        self.panel_1.SetSizer(sizer_3)
+        sizer_3.Fit(self.panel_1)
+        sizer_3.SetSizeHints(self.panel_1)
+        sizer_1.Add(self.panel_1, 1, wxEXPAND, 0)
         self.SetAutoLayout(1)
         self.SetSizer(sizer_1)
         sizer_1.Fit(self)
