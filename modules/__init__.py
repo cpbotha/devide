5e80301f85505712304ba528f9a736eaa817f85d
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-02-06 16:23:40 +0000

    Removed deprecated code.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py by Charl P. Botha <cpbotha@ieee.org>
-# $Id: __init__.py,v 1.12 2003/01/27 20:14:57 cpbotha Exp $
+# $Id: __init__.py,v 1.13 2003/02/06 16:23:39 cpbotha Exp $
 # contains list of built-in modules; update when adding new modules
 # the user_modules get listed automatically
 
@@ -13,7 +13,6 @@ module_list = ['dtsc_seg_flt',
                'vtk_hdf_rdr',
                'vtk_mc_flt',
                'vtk_plydta_rdr',
-               'vtk_slice_vwr',
                'vtk_stl_rdr',
                'vtk_vol_rdr']
 
