f7ef68a5c280908ff48db36ec73f9e3af458a1da
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-02-07 17:35:01 +0000

    Changed DICOM reader name.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,15 +1,15 @@
 # __init__.py by Charl P. Botha <cpbotha@ieee.org>
-# $Id: __init__.py,v 1.13 2003/02/06 16:23:39 cpbotha Exp $
+# $Id: __init__.py,v 1.14 2003/02/07 17:35:01 cpbotha Exp $
 # contains list of built-in modules; update when adding new modules
 # the user_modules get listed automatically
 
-module_list = ['dtsc_seg_flt',
+module_list = ['dicomRDR',
+               'dtsc_seg_flt',
                'doubleThresholdFLT',
                'ghr_linreg_flt',
 	       'slice3d_vwr',
                'vtkStructPtsRDR',               
                'vtkStructPtsWRT',
-               'vtk_dicom_rdr',
                'vtk_hdf_rdr',
                'vtk_mc_flt',
                'vtk_plydta_rdr',
