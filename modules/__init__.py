697f42ce89fa6a70d4add4fa3cb5e2f568fbf704
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-02-13 12:56:35 +0000

    More modules have changed their names.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py by Charl P. Botha <cpbotha@ieee.org>
-# $Id: __init__.py,v 1.14 2003/02/07 17:35:01 cpbotha Exp $
+# $Id: __init__.py,v 1.15 2003/02/13 12:56:35 cpbotha Exp $
 # contains list of built-in modules; update when adding new modules
 # the user_modules get listed automatically
 
@@ -7,12 +7,12 @@ module_list = ['dicomRDR',
                'dtsc_seg_flt',
                'doubleThresholdFLT',
                'ghr_linreg_flt',
+               'hdfRDR',
 	       'slice3d_vwr',
+               'stlRDR',
+               'vtkPolyDataRDR',
                'vtkStructPtsRDR',               
                'vtkStructPtsWRT',
-               'vtk_hdf_rdr',
                'vtk_mc_flt',
-               'vtk_plydta_rdr',
-               'vtk_stl_rdr',
                'vtk_vol_rdr']
 
