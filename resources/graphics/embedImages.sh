fea86c52887bb93b15ea541ae3468af14c2543bc
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-05-20 16:23:04 +0000

    Initial import.

---
+++ resources/graphics/embedImages.sh
@@ -0,0 +1,5 @@
+# embed used images into images.py
+# $Id: embedImages.sh,v 1.1 2003/05/20 16:22:42 cpbotha Exp $
+
+img2py -n dscas3logo64x64 dscas3logo64x64.png images.py
+# continue with img2py -a -n ... so it'll append
