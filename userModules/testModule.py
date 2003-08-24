c99c0d08f17665b59f6fee5cbb26bc995d2b627a
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-08-24 21:28:36 +0000

    Fixed this simple user module.

--- userModules/testModule.py
+++ userModules/testModule.py
@@ -1,5 +1,6 @@
 from moduleBase import moduleBase
 from moduleMixins import noConfigModuleMixin
+import moduleUtils
 from wxPython.wx import *
 import vtk
 
@@ -14,26 +15,21 @@ class testModule(moduleBase, noConfigModuleMixin):
 
         # we'll be playing around with some vtk objects, this could
         # be anything
-        self._testObject0 = vtk.vtkTriangleFilter()
-        self._testObject0b = vtk.vtkWindowedSincPolyDataFilter()
-        self._testObject0b.SetNumberOfIterations(20)
-        self._testObject0b.SetInput(self._testObject0.GetOutput())
-        self._testObject1 = vtk.vtkCurvatures()
-        self._testObject1.SetCurvatureTypeToMean()
-        self._testObject1.SetInput(self._testObject0b.GetOutput())
+        self._triangleFilter = vtk.vtkTriangleFilter()
+        self._curvatures = vtk.vtkCurvatures()
+        self._curvatures.SetCurvatureTypeToMean()
+        self._curvatures.SetInput(self._triangleFilter.GetOutput())
 
-        # following is the standard way of connecting up the dscas3 progress
-        # callback to a VTK object; you should do this for all objects in
-        # your module
-        self._testObject1.SetProgressText('doing stuff...')
-        mm = self._moduleManager
-        self._testObject1.SetProgressMethod(lambda s=self, mm=mm:
-                                            mm.vtk_progress_cb(
-            s._testObject1))
-
-        self._viewFrame = self._createViewFrame('Test Module View',
-                                                {'testObject1' :
-                                                 self._testObject1})
+        moduleUtils.setupVTKObjectProgress(self, self._triangleFilter,
+                                           'Triangle filtering...')
+        moduleUtils.setupVTKObjectProgress(self, self._curvatures,
+                                           'Calculating curvatures...')
+        
+        
+        self._viewFrame = self._createViewFrame({'triangleFilter' :
+                                                 self._triangleFilter,
+                                                 'curvatures' :
+                                                 self._curvatures})
 
     def close(self):
         # we play it safe... (the graph_editor/module_manager should have
@@ -43,20 +39,20 @@ class testModule(moduleBase, noConfigModuleMixin):
         # don't forget to call the close() method of the vtkPipeline mixin
         noConfigModuleMixin.close(self)
         # get rid of our reference
-        del self._testObject1
+        del self._triangleFilter
+        del self._curvatures
 
     def getInputDescriptions(self):
 	return ('vtkPolyData',)
 
     def setInput(self, idx, inputStream):
-        self._testObject0.SetInput(inputStream)
+        self._triangleFilter.SetInput(inputStream)
 
     def getOutputDescriptions(self):
-        return (self._testObject1.GetOutput().GetClassName(),)
-    
+        return (self._curvatures.GetOutput().GetClassName(),)
 
     def getOutput(self, idx):
-        return self._testObject1.GetOutput()
+        return self._curvatures.GetOutput()
 
     def logicToConfig(self):
         pass
@@ -70,12 +66,9 @@ class testModule(moduleBase, noConfigModuleMixin):
 
     def configToView(self):
         pass
-    
 
     def executeModule(self):
-        print "hello alex"
-        self._testObject1.Update()
-
+        self._curvatures.Update()
 
     def view(self, parent_window=None):
         # if the window was visible already. just raise it
