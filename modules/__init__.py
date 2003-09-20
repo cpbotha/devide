d5bda8ee925ab3a9079e84c3e5fec7296985f54a
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-09-20 21:50:18 +0000

    Added all the new-style dscas3 module names.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,37 +1,35 @@
 # __init__.py by Charl P. Botha <cpbotha@ieee.org>
-# $Id: __init__.py,v 1.39 2003/09/19 23:54:16 cpbotha Exp $
+# $Id: __init__.py,v 1.40 2003/09/20 21:50:18 cpbotha Exp $
 # contains list of built-in modules; update when adding new modules
 # the user_modules get listed automatically
 
 # we HAVE to hard-code this list, because all these modules will be frozen
 # along with the application... we can't search directories.
 
-module_list = ['appendPolyDataFLT',
-               'contourFLT',
-               'dicomRDR',
-               'decimateFLT',
-               'doubleThresholdFLT',
-               'glenoidMouldDesignFLT',
-               'hdfRDR',
-               'ifdocRDR',
-               'ifdocVWR',               
-               'marchingCubesFLT',
-               'polyDataConnectFLT',
-               'polyDataNormalsFLT',
-               'rawVolumeRDR',
-               'seedConnectFLT',
-               'shellSplatSimpleFLT',
-	       'slice3dVWR',
-               'stlRDR',
-               'stlWRT',
-               'vtkPolyDataRDR',
-               'vtkPolyDataWRT',
-               'vtkStructPtsRDR',               
-               'vtkStructPtsWRT',
-               'wsMeshSmoothFLT']
-
-moduleList = ['Readers.hdfRDR',
+moduleList = ['Readers.dicomRDR',
+              'Readers.hdfRDR',
+              'Readers.ifdocRDR',
               'Readers.dicomRDR',
-              'Filters.doubleThreshold',
-              'Writers.vtkPolyDataWRT']
+              'Readers.rawVolumeRDR',
+              'Readers.stlRDR',
+              'Readers.vtkPolyDataRDR',
+              'Readers.vtkStructPtsRDR',
 
+              'Viewers.ifdocVWR',
+              'Viewers.slice3dVWR',
+
+              'Filters.appendPolyData',
+              'Filters.contour',
+              'Filters.decimate',
+              'Filters.doubleThreshold',
+              'Filters.glenoidMouldDesign',
+              'Filters.marchingCubes',
+              'Filters.polyDataConnect',
+              'Filters.polyDataNormals',
+              'Filters.seedConnect',
+              'Filters.shellSplatSimple',
+              'Filters.wsMeshSmooth',
+              
+              'Writers.stlWRT',
+              'Writers.vtkPolyDataWRT'
+              'Writers.vtkStructPtsWRT']
