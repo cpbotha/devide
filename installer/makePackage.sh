720120cb371a688806b338afee2072b080818aca
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-10-05 21:41:06 +0000

    Refinements to Installer script: *~ and #*# filen are now deleted before we begin, so that McMillan Tree() TOC statements don't collect these.

--- installer/makePackage.sh
+++ installer/makePackage.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# $Id: makePackage.sh,v 1.3 2003/05/19 12:27:57 cpbotha Exp $
+# $Id: makePackage.sh,v 1.4 2003/10/05 21:41:06 cpbotha Exp $
 
 # go to the directory that contains makePackage.sh (i.e. dscas3/installer)
 cd `dirname $0`
 
 # nuke all .pyc files (to be sure)
 find ../ -name "*.pyc" -exec rm {} \;
+# nuke all backup files
+find ../ -name "*~" -exec rm {} \;
+find ../ -name "#*#" -exec rm {} \;
 
 # remove build directories so that we are assured of a clean start
 rm -rf distdscas3
