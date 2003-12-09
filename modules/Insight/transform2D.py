f4f7bb2aa061b1411b31942b3e0c8ee38d6eef44
Charl P. Botha <cpbotha@users.noreply.github.com>
2003-12-09 12:56:51 +0000

    Initial import.

---
+++ modules/Insight/transform2D.py
@@ -0,0 +1,11 @@
+# $Id: transform2D.py,v 1.1 2003/12/09 12:56:51 cpbotha Exp $
+
+from imageStackRDR import imageStackClass
+from moduleBase import moduleBase
+import InsightToolkit as itk
+
+class transform2D(moduleBase):
+
+    def __init__(self, moduleManager):
+        moduleBase.__init__(self, moduleManager)
+
