0d8ba10b700a2cfc339dc26e9eb431022ca48ae7
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-01-15 10:36:21 +0000

    Changed all to DeVIDE.

--- resources/graphics/embedImages.sh
+++ resources/graphics/embedImages.sh
@@ -1,8 +1,8 @@
 # embed used images into images.py
-# $Id: embedImages.sh,v 1.3 2003/05/20 17:17:02 cpbotha Exp $
+# $Id: embedImages.sh,v 1.4 2004/01/15 10:36:21 cpbotha Exp $
 
-img2py -n dscas3logo64x64 dscas3logo64x64.png images.py
-img2py -a -n dscas3logo32x32 dscas3logo32x32.png images.py
+img2py -n devidelogo64x64 devidelogo64x64.png images.py
+img2py -a -n devidelogo32x32 devidelogo32x32.png images.py
 
-img2py -a -n dscas3logom32x32 dscas3logom32x32.png images.py
+img2py -a -n devidelogom32x32 devidelogom32x32.png images.py
 
