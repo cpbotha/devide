5d8ee34f42b884835428bf288ac7bcd24dbd8d6c
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-02-27 17:19:35 +0000

    Good progress (documentation-wise).

--- modules/Insight/geodesicActiveContour.py
+++ modules/Insight/geodesicActiveContour.py
@@ -1,5 +1,5 @@
 # geodesicActiveContour.py
-# $Id: geodesicActiveContour.py,v 1.1 2004/02/27 17:02:22 cpbotha Exp $
+# $Id: geodesicActiveContour.py,v 1.2 2004/02/27 17:19:35 cpbotha Exp $
 
 #import fixitk as itk
 from moduleBase import moduleBase
@@ -9,6 +9,17 @@ from moduleMixins import scriptedConfigModuleMixin
 #import ConnectVTKITKPython as CVIPy
 
 class geodesicActiveContour(scriptedConfigModuleMixin, moduleBase):
+
+    """Module for performing Geodesic Active Contour-based segmentation on
+    3D data.
+
+    We make use of the following pipelines:<br>
+    1. reader -> smoothing -> gradientMagnitude -> sigmoid -> FI<br>
+    2. fastMarching -> geodesicActiveContour(FI) -> thresholder -> writer<br>
+    The output of pipeline 1 is a feature image that is used by the
+    geodesicActiveContour object.  Also see figure 9.18 in the ITK
+    Software Guide.
+    """
 
     def __init__(self, moduleManager):
 
@@ -23,15 +34,19 @@ class geodesicActiveContour(scriptedConfigModuleMixin, moduleBase):
         
         configList = [
             ('Initial Distance:', 'initialDistance', 'base:float', 'text',
-             'The initial distance.. [TBD]'),
+             'The initial distance of the initial front.'),
             ('Sigma:', 'sigma', 'base:float', 'text',
-             'Sigma.. [TBD'),
+             'Sigma parameter of the Gaussian that will be used to calculate '
+             'the gradient.'),
             ('Sigmoid Alpha:', 'sigmoidAlpha', 'base:float', 'text',
              'Alpha parameter for the sigmoid filter'),
             ('Sigmoid Beta:', 'sigmoidBeta', 'base:float', 'text',
              'Beta parameter for the sigmoid filter'),
             ('Propagation scaling:', 'propagationScaling', 'base:float',
-             'text', 'Propagation scaling')]
+             'text', 'Propagation scaling parameter for the geodesic active '
+             'contour, '
+             'i.e. balloon force.  Positive for outwards, negative for '
+             'inwards.')]
         
         scriptedConfigModuleMixin.__init__(self, configList)
 
