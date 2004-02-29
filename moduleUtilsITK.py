4902d917cef3c4ec73255ee97c763a0a98ef6947
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-02-29 17:51:36 +0000

    I made a fix in itkPyCommand, we don't need the extra binding to the CommandCallable anymore.

--- moduleUtilsITK.py
+++ moduleUtilsITK.py
@@ -1,4 +1,4 @@
-# $Id: moduleUtilsITK.py,v 1.1 2004/02/29 02:29:31 cpbotha Exp $
+# $Id: moduleUtilsITK.py,v 1.2 2004/02/29 17:51:36 cpbotha Exp $
 
 import fixitk as itk
 
@@ -12,10 +12,6 @@ def setupITKObjectProgress(dvModule, obj, nameOfObject, progressText):
         mm.genericProgressCallback(
             nameOfObject, obj.GetProgress(), progressText)
 
-    # we have to store this extra reference: I have to fix itkPyCommand
-    # to do a PyINCREF when obj is assigned!
-    # I also have to check for a non-NULL object in PyExecute.
-    obj.commandCallable = commandCallable
     pc = itk.itkPyCommand_New()
     pc.SetCommandCallable(commandCallable)
     obj.AddObserver(itk.itkProgressEvent(), pc.GetPointer())
