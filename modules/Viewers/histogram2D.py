ed9704dc7f519e9096d2b3871dbaaaaf5e68c8ac
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-03-05 14:22:00 +0000

    Integrated with scriptedConfigModuleMixin.

--- modules/Viewers/histogram2D.py
+++ modules/Viewers/histogram2D.py
@@ -1,11 +1,12 @@
-# $Id: histogram2D.py,v 1.4 2004/02/19 14:36:08 cpbotha Exp $
+# $Id: histogram2D.py,v 1.5 2004/03/05 14:22:00 cpbotha Exp $
 
 from moduleBase import moduleBase
+from moduleMixins import scriptedConfigModuleMixin
 import moduleUtils
 import vtk
 import vtkdevide
 
-class histogram2D(moduleBase):
+class histogram2D(scriptedConfigModuleMixin, moduleBase):
     """This module takes two inputs and creates a 2D histogram with input 1
     vs input 2.
 
@@ -18,7 +19,33 @@ class histogram2D(moduleBase):
         self._histogram = vtkdevide.vtkImageHistogram2D()
         moduleUtils.setupVTKObjectProgress(self, self._histogram,
                                            'Calculating 2D histogram')
-        
+
+
+        self._config.input1Bins = 256
+        self._config.input2Bins = 256
+        self._config.maxSamplesPerBin = 512
+
+        configList = [
+            ('Number of bins for input 1', 'input1Bins', 'base:int', 'text',
+             'The full range of input 1 values will be divided into this many '
+             'classes.'),
+            ('Number of bins for input 2', 'input2Bins', 'base:int', 'text',
+             'The full range of input 2 values will be divided into this many '
+             'classes.'),
+            ('Maximum samples per bin', 'maxSamplesPerBin', 'base:int', 'text',
+             'The number of samples per 2D bin/class will be truncated to '
+             'this value.')]
+
+        scriptedConfigModuleMixin.__init__(self, configList)
+
+        self._viewFrame = self._createWindow(
+            {'Module (self)' : self,
+             'vtkImageHistogram2D' : self._histogram})
+
+        # pass the data down to the underlying logic
+        self.configToLogic()
+        # and all the way up from logic -> config -> view to make sure
+        self.syncViewWithLogic()
 
         self._input0 = None
         self._input1 = None
@@ -80,8 +107,16 @@ class histogram2D(moduleBase):
     def executeModule(self):
         self._histogram.Update()
 
-    def view(self):
-        pass
+    def logicToConfig(self):
+        self._config.input1Bins = self._histogram.GetInput1Bins()
+        self._config.input2Bins = self._histogram.GetInput2Bins()
+        self._config.maxSamplesPerBin = self._histogram.GetMaxSamplesPerBin()
+    
+    def configToLogic(self):
+        self._histogram.SetInput1Bins(self._config.input1Bins)
+        self._histogram.SetInput2Bins(self._config.input2Bins)
+        self._histogram.SetMaxSamplesPerBin(self._config.maxSamplesPerBin)
+
 
     # ----------------------------------------------------------------------
     # non-API methods start here -------------------------------------------
