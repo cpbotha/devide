f028174b84c50653fd5d58f5c3edd749f16f3bce
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-03-07 21:24:14 +0000

    Whitespace removed.

--- defaults.py
+++ defaults.py
@@ -1,9 +1,7 @@
 # file containing some default config values for devide
-# $Id: defaults.py,v 1.4 2004/01/28 23:10:08 cpbotha Exp $
+# $Id: defaults.py,v 1.5 2004/03/07 21:24:14 cpbotha Exp $
 
 # When building Installer packages, this value is examined to determine
 # whether ITK should be packaged along with the rest.  This value is usually
 # overridden by command-line switches.
 USE_INSIGHT = False
-
-
