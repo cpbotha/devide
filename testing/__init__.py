a320d4d9442c754f1cbc2a094adf4a7c4758ed6b
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-03-21 21:02:28 +0000

    Wrote one more test for the confidenceSeedConnect.  It's not really checking for failure yet.

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,5 +1,5 @@
 # testing.__init__.py copyright 2004 by Charl P. Botha http://cpbotha.net/
-# $Id: __init__.py,v 1.3 2004/03/19 11:21:19 cpbotha Exp $
+# $Id: __init__.py,v 1.4 2004/03/21 21:02:28 cpbotha Exp $
 # this drives the devide unit testing.  neat huh?
 
 import os
@@ -17,6 +17,10 @@ class graphEditorTestBase(unittest.TestCase):
         _devideApp._graphEditor.clearAllGlyphsFromCanvas()
 
 class graphEditorVolumeTestBase(graphEditorTestBase):
+    """Uses superQuadric, implicitToVolume and doubleThreshold to create
+    a volume that we can run some tests on.
+    """
+    
     def setUp(self):
         # call parent setUp method
         graphEditorTestBase.setUp(self)
@@ -31,6 +35,11 @@ class graphEditorVolumeTestBase(graphEditorTestBase):
 
         (dtmod, dtglyph) = _devideApp._graphEditor.createModuleAndGlyph(
             10, 130, 'modules.Filters.doubleThreshold')
+
+        # configure the implicitToVolume to have somewhat tighter bounds
+        cfg = ivmod.getConfig()
+        cfg.modelBounds = (-1.0, 1.0, -0.25, 0.25, 0.0, 0.75)
+        ivmod.setConfig(cfg)
 
         # then configure the doubleThreshold with the correct thresholds
         cfg = dtmod.getConfig()
@@ -90,12 +99,67 @@ class graphEditorBasic(graphEditorTestBase):
         
         self.failUnless(ret)
 
+# ----------------------------------------------------------------------------
 class testReadersWriters(graphEditorVolumeTestBase):
     
     def testVTI(self):
         """Testing basic readers/writers.
         """
         self.failUnless(1 == 1)
+
+# ----------------------------------------------------------------------------
+class testITKBasic(graphEditorVolumeTestBase):
+
+    def testConfidenceSeedConnect(self):
+        """Test confidenceSeedConnect and VTK<->ITK interconnect.
+        """
+
+        # this will be the last big created thingy... from now on we'll
+        # do DVNs.  This simulates the user's actions creating the network though.
+
+        # create a slice3dVWR
+        (svmod, svglyph) = _devideApp._graphEditor.createModuleAndGlyph(
+            200, 190, 'modules.Viewers.slice3dVWR')
+
+        # connect up the created volume and redraw
+        ret = _devideApp._graphEditor._connect(self.dtglyph, 0, svglyph, 0)
+        # make sure it can connect
+        self.failUnless(ret)
+
+        # storeCursor wants a 4-tuple and value - we know what these should be
+        svmod.selectedPoints._storeCursor((20,20,0,1))
+
+        # connect up the insight bits
+        (v2imod, v2iglyph) = _devideApp._graphEditor.createModuleAndGlyph(
+            200, 10, 'modules.Insight.VTKtoITKF3')
+
+        (cscmod, cscglyph) = _devideApp._graphEditor.createModuleAndGlyph(
+            200, 70, 'modules.Insight.confidenceSeedConnect')
+
+        (i2vmod, i2vglyph) = _devideApp._graphEditor.createModuleAndGlyph(
+            200, 130, 'modules.Insight.ITKF3toVTK')
+
+        ret = _devideApp._graphEditor._connect(self.dtglyph, 0, v2iglyph, 0)
+        self.failUnless(ret)
+        
+        ret = _devideApp._graphEditor._connect(v2iglyph, 0, cscglyph, 0)
+        self.failUnless(ret)
+        
+        ret = _devideApp._graphEditor._connect(cscglyph, 0, i2vglyph, 0)
+        self.failUnless(ret)
+
+        # there's already something on the 0'th input of the slice3dVWR
+        ret = _devideApp._graphEditor._connect(i2vglyph, 0, svglyph, 1)
+        self.failUnless(ret)
+        
+        # connect up the selected points
+        ret = _devideApp._graphEditor._connect(svglyph, 0, cscglyph, 1)
+        self.failUnless(ret)        
+        
+        # redraw the canvas
+        _devideApp._graphEditor._graphFrame.canvas.redraw()
+
+        
 
 # ----------------------------------------------------------------------------
 class devideTesting:
@@ -109,7 +173,13 @@ class devideTesting:
         self.basicSuite.addTest(graphEditorBasic('testSimpleNetwork'))
         self.basicSuite.addTest(testReadersWriters('testVTI'))
 
-        self.mainSuite = unittest.TestSuite((self.basicSuite,))
+        self.itkSuite = unittest.TestSuite()
+        self.itkSuite.addTest(testITKBasic('testConfidenceSeedConnect'))
+
+        if _devideApp.mainConfig.useInsight:
+            self.mainSuite = unittest.TestSuite((self.basicSuite, self.itkSuite))
+        else:
+            self.mainSuite = unittest.TestSuite((self.basicSuite,))
         
         
     def runAllTests(self):
