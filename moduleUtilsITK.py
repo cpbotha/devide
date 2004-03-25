b63f0c44ebbbdd5f3f85a8e6cb6fe2f106af5d3c
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-03-25 12:32:10 +0000

    ERR: Fixed setupITKObserver so that the Python callable won't contain a direct binding to the object which is being observed.  This would of course keep the object alive, which would keep the observer alive, which would keep the callable alive, etc.

--- moduleUtilsITK.py
+++ moduleUtilsITK.py
@@ -1,4 +1,4 @@
-# $Id: moduleUtilsITK.py,v 1.2 2004/02/29 17:51:36 cpbotha Exp $
+# $Id: moduleUtilsITK.py,v 1.3 2004/03/25 12:32:10 cpbotha Exp $
 
 import fixitk as itk
 
@@ -6,11 +6,30 @@ import fixitk as itk
 
 def setupITKObjectProgress(dvModule, obj, nameOfObject, progressText):
 
+    # first we have to find the attribute of dvModule that binds
+    # to obj.  We _don't_ want to have a binding to obj hanging around
+    # in our callable, because this means that the ITK object can never
+    # be destroyed.  Instead we look up the binding everytime the callable
+    # is invoked by making use of getattr on the devideModule binding.
+
+    # find attribute string of obj in dvModule
+    di = dvModule.__dict__.items()
+    objAttrString = None
+    for key, value in di:
+        if value == obj:
+            objAttrString = key
+            break
+
+    if not objAttrString:
+        raise Exception, 'Could not determine attribute string for ' \
+              'object %s.' % (obj.__class__.__name__)
+
     mm = dvModule._moduleManager
 
     def commandCallable():
         mm.genericProgressCallback(
-            nameOfObject, obj.GetProgress(), progressText)
+            nameOfObject, getattr(dvModule, objAttrString).GetProgress(),
+            progressText)
 
     pc = itk.itkPyCommand_New()
     pc.SetCommandCallable(commandCallable)
