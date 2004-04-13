e3a8e56e975c49d997bf6456e8843be79fc51dac
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-04-13 14:39:22 +0000

    itkBasicFiltersBPython now supported.

--- fixitk/itkBasicFiltersPythonTopLevel.py
+++ fixitk/itkBasicFiltersPythonTopLevel.py
@@ -1,4 +1,11 @@
 from itkCommonPythonTopLevel import *
 __itk_import_data__ = itkBasePythonTopLevel.preimport()
 from ITKBasicFiltersPython import *
+
+# ITKBasicFiltersBPython was added AFTER ITK 1.6
+try:
+    from ITKBasicFiltersBPython import *
+except ImportError:
+    pass
+
 itkBasePythonTopLevel.postimport(__itk_import_data__)
