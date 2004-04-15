86a19c862a860f6f5cb9b6b200cd0fe9bd9bd380
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-04-15 12:51:00 +0000

    Use chrpath to remove rpath information from all libs.

--- installer/makePackage.sh
+++ installer/makePackage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: makePackage.sh,v 1.7 2004/03/20 18:14:01 cpbotha Exp $
+# $Id: makePackage.sh,v 1.8 2004/04/15 12:51:00 cpbotha Exp $
 
 # go to the directory that contains makePackage.sh (i.e. devide/installer)
 cd `dirname $0`
@@ -21,6 +21,8 @@ INSTALLER='python /home/cpbotha/build/Installer/Build.py'
 $INSTALLER devide.spec
 # strip all the libraries
 strip distdevide/*.so
+# remove rpath information (else the installation doesn't work everywhere)
+chrpath --delete distdevide/*.so
 # make a tarball
 mv distdevide devide
 rm -f devide.tar.gz
