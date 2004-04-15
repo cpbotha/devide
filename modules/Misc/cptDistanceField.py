c61765cd0d80fd5fa612e20e758fc7209457c3fe
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-04-15 12:51:28 +0000

    Initial import of CPT distance field module.

---
+++ modules/Misc/cptDistanceField.py
@@ -0,0 +1,159 @@
+import genUtils
+from moduleBase import moduleBase
+import moduleUtils
+from moduleMixins import noConfigModuleMixin
+import os
+import vtk
+
+class cptDistanceField(noConfigModuleMixin, moduleBase):
+    """Driver module for Mauch's CPT code.
+
+    This takes an image data and a mesh input.  The imagedata is only used
+    to determine the bounds of the output distance field.  The mesh
+    is converted to the CPT brep format using the DeVIDE cptBrepWRT module.
+    A geom file is created.  The CPT driver is executed with the geom and
+    brep files.  The output distance field is read, y-axis is flipped, and
+    the whole shebang is made available at the output.
+
+    The distance will be calculated up to _maxDistance.
+
+    NOTE: This module has to be explicitly executed.
+
+    $Revision: 1.1 $
+    """
+
+    _cptDriverExe = '/home/cpbotha/build/cpt/3d/driver/driver.exe'
+
+    def __init__(self, moduleManager):
+
+        # call parent constructor
+        moduleBase.__init__(self, moduleManager)
+        noConfigModuleMixin.__init__(self)
+
+        self._imageInput = None
+        self._meshInput = None
+        self._maxDistance = 5
+
+        
+        self._flipper = vtk.vtkImageFlip()
+        self._flipper.SetFilteredAxis(1)
+        moduleUtils.setupVTKObjectProgress(
+            self, self._flipper, 'Flipping Y axis.')
+
+        self._createViewFrame({'Module (self)' : self})
+        
+        self.configToLogic()
+        # make sure these filter through from the bottom up
+        self.syncViewWithLogic()
+
+    def close(self):
+        # we should disconnect all inputs
+        self.setInput(0, None)
+        self.setInput(1, None)
+        noConfigModuleMixin.close(self)
+
+    def getInputDescriptions(self):
+	return ('VTK Image', 'VTK Polydata')
+    
+    def setInput(self, idx, inputStream):
+        if idx == 0:
+            try:
+                if inputStream == None or inputStream.IsA('vtkImageData'):
+                    self._imageInput = inputStream
+                else:
+                    raise TypeError
+                
+            except (TypeError, AttributeError):
+                raise TypeError, 'This input requires a vtkImageData.'
+                
+        else:
+            try:
+                if inputStream == None or inputStream.IsA('vtkPolyData'):
+                    self._meshInput = inputStream
+                else:
+                    raise TypeError
+                
+            except (TypeError, AttributeError):
+                raise TypeError, 'This input requires a vtkPolyData.'
+            
+        
+    def getOutputDescriptions(self):
+	return ('Distance field (VTK Image)',)
+    
+    def getOutput(self, idx):
+        return self._flipper.GetOutput()
+    
+    def logicToConfig(self):
+        pass
+
+    def configToLogic(self):
+        pass
+
+    def viewToConfig(self):
+        pass
+
+    def configToView(self):
+        pass
+
+    def executeModule(self):
+        if self._imageInput and self._meshInput:
+
+            # basename for all CPT files
+            cptBaseName = os.tempnam()
+
+            # first convert mesh data to brep
+            cbw = self._moduleManager.createModule(
+                'modules.Writers.cptBrepWRT')
+            cbw.setInput(0, self._meshInput)
+            cfg = cbw.getConfig()
+            brepFilename = '%s.brep' % (cptBaseName,)
+            cfg.filename = brepFilename
+            cbw.setConfig(cfg)
+            # we're calling it directly... propagations will propagate
+            # upwards to our caller (the moduleManager) - execution
+            # will be interrupted if cbw flags an error
+            cbw.executeModule()
+
+            # now let's write the geom file
+            self._imageInput.UpdateInformation()
+            b = self._imageInput.GetBounds()
+            d = self._imageInput.GetDimensions()
+
+            geomFile = file('%s.geom' % (cptBaseName,), 'w')
+            # bounds
+            geomFile.write('%f %f %f %f %f %f\n' % (b[0], b[2], b[4],
+                                                    b[1], b[3], b[5]))
+            # dimensions
+            geomFile.write('%d %d %d\n' % (d[0], d[1], d[2]))
+            # maximum distance
+            geomFile.write('%d\n' % (self._maxDistance,))
+            # must be signed
+            geomFile.write('1\n')
+            geomFile.close()
+
+            # now we can call the driver
+            os.system('%s -b -o %s %s.geom %s.brep' % \
+                      (self._cptDriverExe,
+                       cptBaseName, cptBaseName, cptBaseName))
+
+            # we should have cptBaseName.dist waiting...
+            reader = vtk.vtkImageReader()
+            reader.SetFileName('%s.dist' % (cptBaseName,))
+            reader.SetFileDimensionality(3)
+            reader.SetDataScalarType(vtk.VTK_DOUBLE)
+            # 3 doubles in header
+            reader.SetHeaderSize(24)
+            reader.SetDataExtent(self._imageInput.GetWholeExtent())
+            reader.SetDataSpacing(self._imageInput.GetSpacing())
+            moduleUtils.setupVTKObjectProgress(
+                self, reader, 'Reading CPT distance field output.')
+
+            self._flipper.SetInput(reader.GetOutput())
+            self._flipper.GetOutput().UpdateInformation()
+            self._flipper.GetOutput().SetUpdateExtentToWholeExtent()
+            self._flipper.Update()
+
+            self._moduleManager.deleteModule(cbw)
+
+            print "CPT Basename == %s" % (cptBaseName,)
+
