7f17be4a7cc4a9bee22686216a8d6182102d3f34
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-05-23 22:38:39 +0000

    Fixed moduleManager hook so it's not dependent on a drive-letter anymore.

--- installer/hooks/hook-moduleManager.py
+++ installer/hooks/hook-moduleManager.py
@@ -1,10 +1,25 @@
 import os
 import sys
 
-if sys.platform.startswith('win'):
-    sys.path.insert(0, 'g:/work/code/devide/')
-else:
-    sys.path.insert(0, '/home/cpbotha/work/code/devide/')
+# normalize path of this file, get dirname
+hookDir = os.path.dirname(os.path.normpath(__file__))
+# split dirname, select everything except the ending "installer/hooks"
+dd = hookDir.split(os.sep)[0:-2]
+# we have to do this trick, since on windows os.path.join('c:', 'blaat')
+# yields 'c:blaat', i.e. relative to current dir, and we know it's absolute
+dd[0] = '%s%s' % (dd[0], os.sep)
+# turn that into a path again by making use of join (the normpath will take
+# care of redundant slashes on *ix due to the above windows trick)
+devideDir = os.path.normpath(os.path.join(*dd))
+
+# now we've inserted the devideDir into the module path, so
+# import modules should work
+sys.path.insert(0, devideDir)
+
+# if sys.platform.startswith('win'):
+#     sys.path.insert(0, 'g:/work/code/devide/')
+# else:
+#     sys.path.insert(0, '/home/cpbotha/work/code/devide/')
 
 import modules
 
