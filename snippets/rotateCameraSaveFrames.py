c2cdcea5fc6134a20d5c560da87737b0333dd072
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-06-23 15:07:31 +0000

    Added sq thingy.

--- snippets/rotateCameraSaveFrames.py
+++ snippets/rotateCameraSaveFrames.py
@@ -6,7 +6,8 @@
 # 1. right click on a slice3dVWR in the graphEditor
 # 2. select "Mark Module" from the drop-down menu
 # 3. accept "slice3dVWR" as suggestion for the mark index name
-# 4. execute this snippet
+# 4. do the same for your superQuadric (select 'superQuadric' as index name)
+# 5. execute this snippet
 
 import os
 import tempfile
@@ -15,8 +16,9 @@ import wx
 
 # get the slice3dVWR marked by the user
 sv = devideApp.moduleManager.getMarkedModule('slice3dVWR')
+sq = devideApp.moduleManager.getMarkedModule('superQuadric')
 
-if sv:
+if sv and sq:
     # bring the window to the front
     sv.view()
     # make sure it actually happens
@@ -32,6 +34,7 @@ if sv:
     
     camera = sv._threedRenderer.GetActiveCamera()
     for i in range(2 * 36):
+        sq._superquadricSource.SetPhiRoundness(i / 36.0)
         camera.Azimuth(10)
         sv.render3D()
 
