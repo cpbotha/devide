85b9b600c590879f8af86334b759e0a537e26700
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-08-17 14:41:26 +0000

    Muchos work getting histogramSegment going.

--- modules/Viewers/histogramSegment.py
+++ modules/Viewers/histogramSegment.py
@@ -1,6 +1,8 @@
 from moduleBase import moduleBase
 import moduleUtils
 import vtk
+import vtkdevide
+import wx
 
 class histogramSegment(moduleBase):
 
@@ -8,7 +10,11 @@ class histogramSegment(moduleBase):
         moduleBase.__init__(self, moduleManager)
 
         self._createViewFrame()
-        #self._bindEvents()
+        self._grid = self._viewFrame.selectorGrid
+
+        self._bindEvents()
+
+        self._buildPipeline()
 
     def close(self):
         for i in range(len(self.getInputDescriptions())):
@@ -59,12 +65,32 @@ class histogramSegment(moduleBase):
             
             
         if idx == 0:
-            self._input0 = checkTypeAndReturnInput(inputStream)
-            #self._histogram.SetInput1(self._input0)
+            input0 = checkTypeAndReturnInput(inputStream)
+            self._histogram.SetInput1(input0)
 
         elif idx == 1:
-            self._input1 = checkTypeAndReturnInput(inputStream)
-            #self._histogram.SetInput2(self._input1)
+            input1 = checkTypeAndReturnInput(inputStream)
+            self._histogram.SetInput2(input1)
+
+        if self._histogram.GetInput(0) and self._histogram.GetInput(1):
+            if self._ipw == None:
+                # this means we have newly valid input and should setup an ipw
+                self._ipw = vtk.vtkImagePlaneWidget()
+                self._histogram.GetOutput().Update()
+                self._ipw.SetInput(self._histogram.GetOutput())
+                self._ipw.SetInteractor(self._viewFrame.rwi)
+                # normal to the Z-axis
+                self._ipw.SetPlaneOrientation(2)
+                self._ipw.SetSliceIndex(0)
+                self._ipw.On()
+
+        else:
+            # this means at least one input is not valid
+            if self._ipw != None:
+                self._ipw.Off()
+                self._ipw.SetInteractor(None)
+                self._ipw.SetInput(None)
+                self._ipw = None
 
 
     def getOutputDescriptions(self):
@@ -96,6 +122,21 @@ class histogramSegment(moduleBase):
     # END OF API METHODS
     # -------------------------------------------------------
 
+    def _bindEvents(self):
+        wx.grid.EVT_GRID_RANGE_SELECT(
+            self._grid, self._handlerGridRangeSelect)
+
+    def _buildPipeline(self):
+        self._histogram = vtkdevide.vtkImageHistogram2D()
+
+        # make sure the user can't do anything entirely stupid
+        istyle = vtk.vtkInteractorStyleImage()
+        self._viewFrame.rwi.SetInteractorStyle(istyle)
+
+        # we'll use this to keep track of our ImagePlaneWidget
+        self._ipw = None
+
+        
 
     def _createViewFrame(self):
         # create the viewerFrame
@@ -104,6 +145,7 @@ class histogramSegment(moduleBase):
         viewFrame = modules.Viewers.resources.python.histogramSegmentFrames.\
                     viewFrame
 
+        # DeVIDE takes care of the icon and the window close handlers
         self._viewFrame = moduleUtils.instantiateModuleViewFrame(
             self, self._moduleManager, viewFrame)
         
@@ -112,8 +154,51 @@ class histogramSegment(moduleBase):
         self._renderer.SetBackground(0.5, 0.5, 0.5)
         self._viewFrame.rwi.GetRenderWindow().AddRenderer(
             self._renderer)
+
+        # add the ECASH buttons
+        moduleUtils.createECASButtons(self, self._viewFrame,
+                                      self._viewFrame.viewFramePanel)
         
         # display the window
         self._viewFrame.Show(True)
         
+    def _handlerGridRangeSelect(self, event):
+        """This event handler is a fix for the fact that the row
+        selection in the wxGrid is deliberately broken.  It's also
+        used to activate and deactivate relevant menubar items.
+        
+        Whenever a user clicks on a cell, the grid SHOWS its row
+        to be selected, but GetSelectedRows() doesn't think so.
+        This event handler will travel through the Selected Blocks
+        and make sure the correct rows are actually selected.
+        
+        Strangely enough, this event always gets called, but the
+        selection is empty when the user has EXPLICITLY selected
+        rows.  This is not a problem, as then the GetSelectedRows()
+        does return the correct information.
+        """
+
+        # both of these are lists of (row, column) tuples
+        tl = self._grid.GetSelectionBlockTopLeft()
+        br = self._grid.GetSelectionBlockBottomRight()
+
+        # this means that the user has most probably clicked on the little
+        # block at the top-left corner of the grid... in this case,
+        # SelectRow has no frikking effect (up to wxPython 2.4.2.4) so we
+        # detect this situation and clear the selection (we're going to be
+        # selecting the whole grid in anycase.
+        if tl == [(0,0)] and br == [(self._grid.GetNumberRows() - 1,
+                                     self._grid.GetNumberCols() - 1)]:
+            self._grid.ClearSelection()
+
+        for (tlrow, tlcolumn), (brrow, brcolumn) in zip(tl, br):
+            for row in range(tlrow,brrow + 1):
+                self._grid.SelectRow(row, True)
+
+        if self._grid.GetSelectedRows():
+            for mi in self._disableMenuItems:
+                mi.Enable(True)
+        else:
+            for mi in self._disableMenuItems:
+                mi.Enable(False)
 
