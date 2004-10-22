0fccdd66b1f12d27f6efb83d0ee47ea028ee6ca4
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-10-22 14:41:26 +0000

    LUT table range now adapts to data scalar range.

--- snippets/changeSVLut.py
+++ snippets/changeSVLut.py
@@ -1,16 +1,19 @@
-# EXPERIMENTAL.  DO NOT USE UNLESS YOU ARE JORIK.
+# EXPERIMENTAL.  DO NOT USE UNLESS YOU ARE JORIK. (OR JORIS)
 
 import vtk
 
-sv = devideApp.moduleManager.getMarkedModule('slice3dVWR')
-
-if sv:
-    ipw = sv.sliceDirections._sliceDirectionsDict.values()[0]._ipws[0]
-    #print ipw.GetLookupTable().GetWindow()
+className = obj.__class__.__name__
+if className == 'slice3dVWR':
+    ipw = obj.sliceDirections._sliceDirectionsDict.values()[0]._ipws[0]
     
+    if ipw.GetInput():
+        mins, maxs = ipw.GetInput().GetScalarRange()
+    else:
+        mins, maxs = -2100, 3000
+        
     lut = vtk.vtkLookupTable()
-    lut.SetTableRange(-2100, 3000)
-    lut.SetHueRange(0.1, 1)
+    lut.SetTableRange(mins, maxs)
+    lut.SetHueRange(0.1, 1.0)
     lut.SetSaturationRange(1.0, 1.0)
     lut.SetValueRange(1.0, 1.0)
     lut.SetAlphaRange(1.0, 1.0)
