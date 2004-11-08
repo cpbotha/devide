47dfa064005eb2723ec66cc9a8482aab5e40b481
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-11-08 13:06:49 +0000

    Fixed unexplained installer error on Linux.

--- defaults.py
+++ defaults.py
@@ -1,7 +1,7 @@
 # file containing some default config values for devide
-# $Id: defaults.py,v 1.10 2004/06/21 16:30:20 cpbotha Exp $
+# $Id: defaults.py,v 1.11 2004/11/08 13:06:48 cpbotha Exp $
 
 # When building Installer packages, this value is examined to determine
 # whether ITK should be packaged along with the rest.  This value is usually
 # overridden by command-line switches.
-USE_INSIGHT = True
+USE_INSIGHT = False
