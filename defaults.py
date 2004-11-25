489bfccba4e3bf761ee7195ad294717a2aae7531
Charl P. Botha <cpbotha@users.noreply.github.com>
2004-11-25 13:09:05 +0000

    I don't know what's default anymore.

--- defaults.py
+++ defaults.py
@@ -1,8 +1,9 @@
 # file containing some default config values for devide
-# $Id: defaults.py,v 1.13 2004/11/20 15:23:16 cpbotha Exp $
+# $Id: defaults.py,v 1.14 2004/11/25 13:09:05 cpbotha Exp $
 
 # When building Installer packages, this value is examined to determine
 # whether ITK should be packaged along with the rest.  This value is usually
 # overridden by command-line switches.
-USE_INSIGHT = False
+USE_INSIGHT = True
+
 
