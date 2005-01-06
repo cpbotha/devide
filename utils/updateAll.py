c335063cf99ed5810ab458c65c6ae097c9101cdb
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-01-06 21:09:36 +0000

    This now works for all dependencies except ConnectVTKITK.

--- utils/updateAll.py
+++ utils/updateAll.py
@@ -1,82 +1,81 @@
 # updates all DeVIDE dependencies except ITK and VTK
 # i.e. do a cvs update, a cmake and a build of:
 # vtkdevide, vtktud, wrapitk and, depending on ITK: wrapitk, ConnectVTKITK
-# $Id: updateAll.py,v 1.1 2005/01/06 18:27:52 cpbotha Exp $
+# $Id: updateAll.py,v 1.2 2005/01/06 21:09:36 cpbotha Exp $
 # authored by Charl P. Botha http://cpbotha.net/
 
 import os
 import sys
 
-# relative paths for all these thingies
-# need source and bin dirs!
-devideTopLevel = '../'
-vtkdevideTopLevel = '../../vtkdevide/'
-vtktudTopLevel = '../../vtktud/'
-wrapitkTopLevel = '../../wrapitk-gcc/'
+# There should be an updateAllDefaults.py file in the same directory
+# as this script.  An example would be:
+# --- BEGIN updateAllDefaults.py
+# devideTopLevel = '/home/cpbotha/work/code/devide'
+# vtkdevideSource = '/home/cpbotha/work/code/vtkdevide'
+# vtkdevideBinary = '/home/cpbotha/work/code/vtkdevide'
+# vtktudSource = '/home/cpbotha/work/code/vtktud'
+# vtktudBinary = '/home/cpbotha/work/code/vtktud'
+# wrapitkSource = '/home/cpbotha/work/code/wrapitk'
+# wrapitkBinary = '/home/cpbotha/work/code/wrapitk-gcc'
+# --- END updateAllDefaults
+try:
+    import updateAllDefaults as defaults
+except ImportError, e:
+    print 'Error loading updateAllDefaults.py: %s' % (str(e),)
+    sys.exit()
 
-# buildcommand
+# buildcommands for posix and MSVS 7.1 on Windows
 if os.name == 'posix':
     buildCommand = 'make'
 else:
-    buildCommand = './msBuild.bat'
+    # first %s is to be replaced by the .sln file
+    buildCommand = 'devenv %s /project ALL_BUILD /projectconfig ' \
+                   '"RelWithDebInfo|Win32" /build RelWithDebInfo'
 
-def updatedevide(ourpath):
-    print 'DEVIDE ---'
-    print 'CVS updating'
-    os.chdir(os.path.join(ourpath, devideTopLevel))
-    os.system('cvs update -dAP')
+cvsUpdateCommand = 'cvs -z3 update -dAP'
 
-def updatevtkdevide(ourpath):
-    print 'VTKDEVIDE ---'
+def updatedevide():
+    print 'CVS updating %s' % (defaults.devideTopLevel,)
+    os.chdir(defaults.devideTopLevel)
+    os.system(cvsUpdateCommand)
 
-    print 'CVS updating'
-    os.chdir(os.path.join(ourpath, vtkdevideTopLevel))
-    os.system('cvs update -dAP')
+def standardUpdateAndBuild(source, binary, slnFile):
+    print 'CVS updating %s' % (source,)
+    os.chdir(source)
+    os.system(cvsUpdateCommand)
 
-    print 'Configuring build'
+    print 'Updating build config in %s' % (binary,)
+    os.chdir(binary)
     os.system('cmake .')
 
-    print 'Building'
-    os.system(buildCommand)
 
-def updatevtktud(ourpath):
-    print 'VTKTUD ---'
+    if os.name == 'posix':
+        theBuildCommand = buildCommand
+    else:
+        theBuildCommand = buildCommand % (slnFile,)
 
-    print 'CVS updating'
-    os.chdir(os.path.join(ourpath, vtktudTopLevel))
-    os.system('cvs update -dAP')
+    print 'Building with "%s"' % (theBuildCommand,)
 
-    print 'Configuring build'
-    os.system('cmake .')
-
-    print 'Building'
-    os.system(buildCommand)
-
-def updatewrapitk(ourpath):
-    print 'WRAPITK ---'
-
-    print 'CVS updating'
-    os.chdir(os.path.join(ourpath, wrapitkTopLevel))
-    os.system('cvs update -dAP')
-
-    print 'Configuring build'
-    os.system('cmake .')
-
-    print 'Building'
-    os.system(buildCommand)
+    os.system(theBuildCommand)
     
+
+def updatevtkdevide():
+    standardUpdateAndBuild(defaults.vtkdevideSource, defaults.vtkdevideBinary,
+                           'vtkdevide.sln')
+
+def updatevtktud():
+    standardUpdateAndBuild(defaults.vtktudSource, defaults.vtktudBinary,
+                           'vtktud.sln')
     
+def updatewrapitk():
+    standardUpdateAndBuild(defaults.wrapitkSource, defaults.wrapitkBinary,
+                           'wrapitk.sln')
 
 def main():
-    # get our canonical path
-    ourpath = os.path.abspath(os.path.dirname(sys.argv[0]))
-    print 'ourpath == %s' % (ourpath,)
-
-    updatedevide(ourpath)
-    updatevtkdevide(ourpath)
-    updatevtktud(ourpath)
-    updatewrapitk(ourpath)
-
+    updatedevide()
+    updatevtkdevide()
+    updatevtktud()
+    updatewrapitk()
 
 if __name__ == '__main__':
     main()
