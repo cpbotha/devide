7b0cee92c331f860b328041771c064dac9741a70
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-05-12 15:31:05 +0000

    Updated: we're now on the latest VTK and ITK.

--- fixitk/itkCommonPythonTopLevel.py
+++ fixitk/itkCommonPythonTopLevel.py
@@ -1,4 +1,5 @@
 from vxlNumericsPythonTopLevel import *
 __itk_import_data__ = itkBasePythonTopLevel.preimport()
-from ITKCommonPython import *
+from ITKCommonAPython import *
+from ITKCommonBPython import *
 itkBasePythonTopLevel.postimport(__itk_import_data__)
