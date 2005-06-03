7f1fa69828194e1624cb90793b943f5ba2b01e3a
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-06-03 22:35:36 +0000

    ENH: Installer seems to work after major wxPython 2.6 reworking.

--- defaults.py
+++ defaults.py
@@ -1,9 +1,9 @@
 # file containing some default config values for devide
-# $Id: defaults.py,v 1.16 2005/05/27 14:46:09 cpbotha Exp $
+# $Id: defaults.py,v 1.17 2005/06/03 22:35:36 cpbotha Exp $
 
 # When building Installer packages, this value is examined to determine
 # whether ITK should be packaged along with the rest.  This value is usually
 # overridden by command-line switches.
-USE_INSIGHT = True 
+USE_INSIGHT = False
 
 
