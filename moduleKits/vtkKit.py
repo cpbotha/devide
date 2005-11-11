cc0b76cba3e4d2b9c84f0332bc5ec75da8c95638
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-11-11 16:17:26 +0000

    Seems to work.  Still refining.

--- moduleKits/vtkKit.py
+++ moduleKits/vtkKit.py
@@ -1,3 +1,5 @@
+# $Id: vtkKit.py,v 1.2 2005/11/11 16:17:26 cpbotha Exp $
+
 import types
 import sys
 
@@ -49,11 +51,88 @@ class vtkModuleTrappedErrors(types.ModuleType):
             # for everything else, thunk back to the default
             return object.__getattribute__(self, name)
 
+def preImportVTK(progressMethod):
+    vtkImportList = [('vtk.common', 'Loading VTK Common.'),
+                     ('vtk.filtering', 'Loading VTK Filtering.'),
+                     ('vtk.io', 'Loading VTK IO.'),
+                     ('vtk.imaging', 'Loading VTK Imaging.'),
+                     ('vtk.graphics', 'Loading VTK Graphics.'),
+                     ('vtk.rendering', 'Loading VTK Rendering.'),
+                     ('vtk.hybrid', 'Loading VTK Hybrid.'),
+                     #('vtk.patented', 'Loading VTK Patented.'),
+                     ('vtk', 'Loading other VTK symbols')]
+
+    # set the dynamic loading flags.  If we don't do this, we get strange
+    # errors on 64 bit machines.  To see this happen, comment this statement
+    # and then run the VTK->ITK connection test case.
+    oldflags = setDLFlags()
+
+    percentStep = 100.0 / len(vtkImportList)
+    currentPercent = 0.0
+
+    # do the imports
+    for module, message in vtkImportList:
+        currentPercent += percentStep
+        progressMethod(currentPercent, message, noTime=True)
+        exec('import %s' % (module,))
+
+    # restore previous dynamic loading flags
+    resetDLFlags(oldflags)
+
+def setDLFlags():
+    # brought over from ITK Wrapping/CSwig/Python
+
+    # Python "help(sys.setdlopenflags)" states:
+    #
+    # setdlopenflags(...)
+    #     setdlopenflags(n) -> None
+    #     
+    #     Set the flags that will be used for dlopen() calls. Among other
+    #     things, this will enable a lazy resolving of symbols when
+    #     importing a module, if called as sys.setdlopenflags(0) To share
+    #     symbols across extension modules, call as
+    #
+    #     sys.setdlopenflags(dl.RTLD_NOW|dl.RTLD_GLOBAL)
+    #
+    # GCC 3.x depends on proper merging of symbols for RTTI:
+    #   http://gcc.gnu.org/faq.html#dso
+    #
+    try:
+        import dl
+        newflags = dl.RTLD_NOW|dl.RTLD_GLOBAL
+    except:
+        newflags = 0x102  # No dl module, so guess (see above).
+        
+    try:
+        oldflags = sys.getdlopenflags()
+        sys.setdlopenflags(newflags)
+    except:
+        oldflags = None
+
+    return oldflags
+
+def resetDLFlags(data):
+    # brought over from ITK Wrapping/CSwig/Python    
+    # Restore the original dlopen flags.
+    try:
+        sys.setdlopenflags(data)
+    except:
+        pass
+
 def init(theModuleManager):
+    # first do the VTK pre-imports: this is here ONLY to keep the user happy
+    # it's not necessary for normal functioning
+    preImportVTK(theModuleManager.setProgress)
+    
     # finally import VTK by first instantiating our new ModuleType class
     vtk = vtkModuleTrappedErrors('vtk')
     # installing it in the modules directory
     sys.modules['vtk'] = vtk
     # and then requesting a reload
     reload(vtk)
+
+    # and do the same for vtkdevide, vtktud
+    vtkdevide = vtkModuleTrappedErrors('vtkdevide')
+    sys.modules['vtkdevide'] = vtkdevide
+    reload(vtkdevide)
 
