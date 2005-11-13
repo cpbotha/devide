7c13211c1283badffd26629b14e51f2b59e0b8c5
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-11-13 17:32:29 +0000

    Changed RemoveAllProps to RemoveAllViewProps.

--- modules/Viewers/slice3dVWR.py
+++ modules/Viewers/slice3dVWR.py
@@ -1,5 +1,5 @@
 # slice3d_vwr.py copyright (c) 2002 Charl P. Botha <cpbotha@ieee.org>
-# $Id: slice3dVWR.py,v 1.47 2005/11/04 15:47:42 cpbotha Exp $
+# $Id: slice3dVWR.py,v 1.48 2005/11/13 17:32:29 cpbotha Exp $
 # next-generation of the slicing and dicing devide module
 
 # TODO: 'refresh' handlers in setInput()
@@ -48,7 +48,7 @@ class slice3dVWR(introspectModuleMixin, colourDialogMixin, moduleBase):
     Please see the main DeVIDE help/user manual by pressing F1.  This module,
     being so absolutely great, has its own section.
 
-    $Revision: 1.47 $
+    $Revision: 1.48 $
     """
 
     IS_VIEW = 1
@@ -184,7 +184,7 @@ class slice3dVWR(introspectModuleMixin, colourDialogMixin, moduleBase):
         del self._cInteractorStyle
 
         # make SURE there are no props in the Renderer
-        self._threedRenderer.RemoveAllProps()
+        self._threedRenderer.RemoveAllViewProps()
         # take care of all our bindings to renderers
         del self._threedRenderer
 
