92c921a6bb7f632d7a5b6aa33fb6b8237a6ebeb9
Charl P. Botha <cpbotha@users.noreply.github.com>
2005-11-17 14:36:14 +0000

    Initial import.

---
+++ modules/Viewers/moduleIndex.py
@@ -0,0 +1,10 @@
+# $Id: moduleIndex.py,v 1.1 2005/11/17 14:36:14 cpbotha Exp $
+
+class histogram1D:
+    kits = ['vtkKit']
+    cats = ['Viewers', 'Statistics']
+
+class slice3dVWR:
+    kits = ['vtkKit']
+    cats = ['Viewers']
+
