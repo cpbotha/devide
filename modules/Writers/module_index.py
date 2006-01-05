ba3ad4b056d7bd08d591b1a53bad28c18424344b
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-05 15:24:27 +0000

    Initial import.

---
+++ modules/Writers/module_index.py
@@ -0,0 +1,40 @@
+class cptBrepWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class __init__:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class ivWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class metaImageWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class pngWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class stlWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class vtiWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class vtkPolyDataWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class vtkStructPtsWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
+class vtpWRT:
+    kits = ['vtkKit']
+    cats = ['Filters']
+
