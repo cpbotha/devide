af779ab6fcbda0d9e08f34268122c68de76f357d
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-12 17:21:23 +0000

    * Documentation added. * Variable names changed to (correct) underscore convention.

--- moduleKits/__init__.py
+++ moduleKits/__init__.py
@@ -1,18 +1,19 @@
 # moduleKits __init__.py
-# $Id: __init__.py,v 1.7 2006/01/12 13:19:21 cpbotha Exp $
+# $Id$
 
 """Top-level __init__ of the module_kits.
 
-@ivar module_kits_list:
-@ivar crucial_kits: 
+@ivar module_kits_list: All moduleKits in this list will have their init()s
+called with the moduleManager as parameter (after being imported).  Before
+this happens though, members of this list that are also in the no-kits list
+(defined by defaults.py or command-line) will be removed.
+
+@ivar crucial_kits: Usually, when a kit raises an exception during its init()
+call, this is ignored and DeVIDE starts up as usual.  However, if the module
+is in the crucial_kit_list, DeVIDE will refuse to start up.
 """
 
-# all moduleKits in this list will have their init()s called with the
-# moduleManager as parameter (after being imported)
-# before this happens though, members of this list that are also in the
-# no-kits list (defined by defaults.py or command-line) will be removed
-moduleKitList = ['vtkKit', 'itkKit']
+module_kit_list = ['vtkKit', 'itkKit']
 
-
-crucialKits = ['vtkKit']
+crucial_kit_list = ['vtkKit']
 
