c0a9f64c68e4c87c9e13a35e9e19e8d96f32574b
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-12 13:19:21 +0000

    initial addition of crucial_kits.

--- moduleKits/__init__.py
+++ moduleKits/__init__.py
@@ -1,10 +1,18 @@
 # moduleKits __init__.py
-# $Id: __init__.py,v 1.6 2005/11/20 21:54:17 cpbotha Exp $
-# modify this file when you add a new moduleKit: add the kitname to the list
-# of import and to the moduleKitList variable.
+# $Id: __init__.py,v 1.7 2006/01/12 13:19:21 cpbotha Exp $
+
+"""Top-level __init__ of the module_kits.
+
+@ivar module_kits_list:
+@ivar crucial_kits: 
+"""
 
 # all moduleKits in this list will have their init()s called with the
 # moduleManager as parameter (after being imported)
 # before this happens though, members of this list that are also in the
 # no-kits list (defined by defaults.py or command-line) will be removed
 moduleKitList = ['vtkKit', 'itkKit']
+
+
+crucialKits = ['vtkKit']
+
