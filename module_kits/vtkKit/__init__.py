4fa883cde8a271e4929b11fe3b913ad0ceecdcc8
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-18 13:26:08 +0000

    Removed automatic hacky error-handling from vtkKit. Added utility functions to do error handling for vtk objects to utils.

--- module_kits/vtkKit/__init__.py
+++ module_kits/vtkKit/__init__.py
@@ -22,73 +22,6 @@ import types
 VERSION = ''
 VTK_VERSION_EXTRA = '-D'
 
-class vtkModuleTrappedErrors(types.ModuleType):
-    """Class to enable default instantiation of all wrapped VTK classes with
-    ErrorEvent observers.
-
-    @author: Charl P. Botha <http://cpbotha.net/>
-    """
-
-    def __init__(self, name, moduleManager):
-        types.ModuleType.__init__(self, name)
-        self._moduleManager = moduleManager
-        
-    def ErrorEventHandler(self, theObject, eventName, callData):
-        """Standard handler for DeVIDE vtkKit object ErrorEvents.
-
-        Due to the Python wrappings, an exception raised here will not
-        go through the normal catch handling, but will instead just
-        be printed to stdout.  Instead, we use moduleManager functionality
-        to indicate the error.  moduleManager will trap these errors
-        and handle them as it usually does.
-        """
-
-        # we only have to report the specific VTK object error,
-        # moduleManager will add information about which DeVIDE module
-        # contains the error
-        self._moduleManager.addNoExcModuleExecError(callData)
-        # debug output
-        print callData
-
-    ErrorEventHandler.CallDataType = "string0"
-    
-    def __getattribute__(self, name):
-        if name.startswith('vtk'):
-            klass = object.__getattribute__(self, name)
-            
-            # dynamically-created class to wrap vtk_object ##################
-            class vtkClassTrappedErrors(klass):
-                def __init__(zelf):
-                    # if our base class has a ctor, call it
-                    try:
-                        rv = klass.__init__(zelf)
-                    except AttributeError, e:
-                        rv = None
-
-                    if hasattr(zelf, 'AddObserver'):
-                        # add the ErrorEvent observer
-                        zelf.AddObserver(
-                            'ErrorEvent', self.ErrorEventHandler)
-
-                    # in the case of a vtkAlgorithm, the Executive has to
-                    # be instrumented as well
-                    if hasattr(zelf, 'GetExecutive'):
-                        zelf.GetExecutive().AddObserver(
-                            'ErrorEvent', self.ErrorEventHandler)
-                    
-                    # and return either the value of the parent constructor
-                    # or none if there was no parent.
-                    return rv
-
-            # end of dynamically-created class ############################
-
-            # return our instrumented class instead of the native vtkclass
-            return vtkClassTrappedErrors
-        
-        else:
-            # for everything else, thunk back to the default
-            return object.__getattribute__(self, name)
-
 def preImportVTK(progressMethod):
     vtkImportList = [('vtk.common', 'VTK Common.'),
                      ('vtk.filtering', 'VTK Filtering.'),
@@ -162,13 +95,9 @@ def init(theModuleManager):
     # first do the VTK pre-imports: this is here ONLY to keep the user happy
     # it's not necessary for normal functioning
     preImportVTK(theModuleManager.setProgress)
-    
-    # finally import VTK by first instantiating our new ModuleType class
-    vtk = vtkModuleTrappedErrors('vtk', theModuleManager)
-    # installing it in the modules directory
-    sys.modules['vtk'] = vtk
-    # and then requesting a reload
-    reload(vtk)
+
+    # import the main module itself
+    import vtk
 
     # and do the same for vtkdevide, vtktud
     vtkdevide = vtkModuleTrappedErrors('vtkdevide', theModuleManager)
@@ -177,6 +106,7 @@ def init(theModuleManager):
 
     # load up some generic functions into this namespace
     import misc
+    import utils
 
     # setup the kit version
     global VERSION
