692156df85675025c5fd1b5338ac9ea10ed2ba13
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-23 14:56:45 +0000

    Fixed naming and removed constructor.

--- module_kits/vtk_kit/mixins.py
+++ module_kits/vtk_kit/mixins.py
@@ -15,18 +15,22 @@ class vtk_error_info_class:
         self.call_data = None
 
 class vtk_error_func_mixin:
-    def __init__(self):
-        self.vtk_error_info = vtk_error_info_class()
-        self.vtk_error_info.error = False
+    def _ensure_vtk_error_info(self):
+        if not hasattr(self, 'vtk_error_info'):
+            self.vtk_error_info = vtk_error_info_class()
+            self.vtk_error_info.error = False
 
-    def add_error_handler(self, vtk_object):
-        utils.add_error_handler(vtk_object, self.error_func)
+    def add_vtk_error_handler(self, vtk_object):
+        self._ensure_vtk_error_info()
+        utils.add_error_handler(vtk_object, self._vtk_error_func)
 
-    def check_error(self):
+    def check_vtk_error(self):
+        self._ensure_vtk_error_info()
         if self.vtk_error_info.error:
             raise RuntimeError(self.vtk_error_info.call_data)
     
-    def error_func(self, vtk_object, event_name, call_data):
+    def _vtk_error_func(self, vtk_object, event_name, call_data):
+        self._ensure_vtk_error_info()
         self.vtk_error_info.error = True
         self.vtk_error_info.object = vtk_object
         self.vtk_error_info.event_name = event_name
