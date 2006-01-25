54e33481d5960153e47193eaf6b247e63df7db62
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-01-25 16:45:50 +0000

    Various changes to see how high we can get pylint to go.

--- module_kits/vtk_kit/mixins.py
+++ module_kits/vtk_kit/mixins.py
@@ -5,31 +5,76 @@
 @author: Charl P. Botha <http://cpbotha.net/>
 """
 
+__revision__ = '$Revision$'
+
 import utils
 
-class vtk_error_info_class:
+class VTKErrorInfoClass:
+    """Class used to encapsulate error information in the VTKErrorFuncMixin
+    mixin class.
+    """
+    
     def __init__(self):
         self.error = False
         self.object = None
         self.event_name = None
         self.call_data = None
 
-class vtk_error_func_mixin:
+class VTKErrorFuncMixin:
+    """Mixin class that can be used by DeVIDE modules that want to do
+    error handling for VTK objects.
+
+    To use this, add it as mixin to your DeVIDE module.   For each VTK object
+    that you instantiate, call self.add_vtk_error_handler(vtk_object) once.
+    Each time that you execute an object, call self.check_vtk_error().  If an
+    error has occurred, an exception will be raised with the appropriate VTK
+    error message.
+    """
+    
+    def __init__(self):
+        pass
+    
     def _ensure_vtk_error_info(self):
+        """Guard function that's called by all methods of this class to ensure
+        the existence of the info instance.  We do it like this so that the
+        user of this mixin does not have to call our constructor.
+        """
+        
         if not hasattr(self, 'vtk_error_info'):
-            self.vtk_error_info = vtk_error_info_class()
+            self.vtk_error_info = VTKErrorInfoClass()
             self.vtk_error_info.error = False
 
     def add_vtk_error_handler(self, vtk_object):
+        """Instrument a vtk_object with an error observer.
+
+        If any such an object experiences an error, the next call to
+        check_vtk_error() will raise an exception with the relevant error
+        message.
+
+        @param vtk_object: The vtk object instance that should get the error
+        observer.
+        """
+        
         self._ensure_vtk_error_info()
         utils.add_error_handler(vtk_object, self._vtk_error_func)
 
     def check_vtk_error(self):
+        """Check and raise error if relevant.
+
+        This will check for the most recent error in one of the VTK objects
+        that you've instrumented with add_vtk_error_handler().  If an error
+        occurred, an exception will be raised with the pertinent VTK error
+        message.
+        """
+        
         self._ensure_vtk_error_info()
         if self.vtk_error_info.error:
             raise RuntimeError(self.vtk_error_info.call_data)
     
     def _vtk_error_func(self, vtk_object, event_name, call_data):
+        """Default observer function used by add_vtk_error_handler().
+        """
+
         self._ensure_vtk_error_info()
         self.vtk_error_info.error = True
         self.vtk_error_info.object = vtk_object
