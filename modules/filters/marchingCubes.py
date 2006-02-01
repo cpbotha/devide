febe0601f1d0742a684fe7ce77ade2e7e0125565
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-02-01 17:02:21 +0000

    Converted to new-style error-handling and scripted config.

--- modules/filters/marchingCubes.py
+++ modules/filters/marchingCubes.py
@@ -1,7 +1,73 @@
-from modules.Filters.sharedModules.contourFLTBase import contourFLTBase
+from moduleBase import moduleBase
+from moduleMixins import scriptedConfigModuleMixin
+import moduleUtils
+import vtk
+from module_kits.vtk_kit.mixins import VTKErrorFuncMixin
 
-class marchingCubes(contourFLTBase):
+class marchingCubes(scriptedConfigModuleMixin, moduleBase, VTKErrorFuncMixin):
 
     def __init__(self, moduleManager):
-        contourFLTBase.__init__(self, moduleManager, 'marchingCubes')
+
+        # call parent constructor
+        moduleBase.__init__(self, moduleManager)
+
+        self._contourFilter = vtk.vtkMarchingCubes()
+
+        moduleUtils.setupVTKObjectProgress(self, self._contourFilter,
+                                           'Extracting iso-surface')
+        self.add_vtk_error_handler(self._contourFilter)
+
+        # now setup some defaults before our sync
+        self._config.iso_value = 128
+
+        config_list = [
+            ('ISO value:', 'iso_value', 'base:float', 'text',
+             'Surface will pass through points with this value.')]
+        scriptedConfigModuleMixin.__init__(self, config_list)
+
+        self._viewFrame = self._createWindow(
+            {'Module (self)' : self,
+             'vtkMarchingCubes' : self._contourFilter})
+
+        # pass the data down to the underlying logic
+        self.configToLogic()
+        # and all the way up from logic -> config -> view to make sure
+        self.logicToConfig()
+        self.configToView()
+        
+    def close(self):
+        # we play it safe... (the graph_editor/module_manager should have
+        # disconnected us by now)
+        self.setInput(0, None)
+
+        # this will take care of all display thingies
+        scriptedConfigModuleMixin.close(self)
+
+        moduleBase.close(self)
+        
+        # get rid of our reference
+        del self._contourFilter
+
+    def getInputDescriptions(self):
+	return ('vtkImageData',)
+
+    def setInput(self, idx, inputStream):
+        self._contourFilter.SetInput(inputStream)
+
+    def getOutputDescriptions(self):
+	return (self._contourFilter.GetOutput().GetClassName(),)
+    
+
+    def getOutput(self, idx):
+        return self._contourFilter.GetOutput()
+
+    def logicToConfig(self):
+        self._config.iso_value = self._contourFilter.GetValue(0)
+
+    def configToLogic(self):
+        self._contourFilter.SetValue(0, self._config.iso_value)
+
+    def executeModule(self):
+        self._contourFilter.Update()
+        self.check_vtk_error()
 
