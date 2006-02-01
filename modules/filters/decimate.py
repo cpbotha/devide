e10bc937ba96df75c63fdc44d1dc34de1b7b7d4e
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-02-01 16:05:09 +0000

    * Converted to new-style error handling. * Converted to scriptedConfigModuleMixin.

--- modules/filters/decimate.py
+++ modules/filters/decimate.py
@@ -4,11 +4,12 @@
 
 import genUtils
 from moduleBase import moduleBase
-from moduleMixins import vtkPipelineConfigModuleMixin
+from moduleMixins import scriptedConfigModuleMixin
 import moduleUtils
 import vtk
+from module_kits.vtk_kit.mixins import VTKErrorFuncMixin
 
-class decimate(moduleBase, vtkPipelineConfigModuleMixin):
+class decimate(scriptedConfigModuleMixin, moduleBase, VTKErrorFuncMixin):
 
     def __init__(self, moduleManager):
 
@@ -24,19 +25,28 @@ class decimate(moduleBase, vtkPipelineConfigModuleMixin):
 
         moduleUtils.setupVTKObjectProgress(self, self._triFilter,
                                            'Converting to triangles')
+        self.add_vtk_error_handler(self._triFilter)
         moduleUtils.setupVTKObjectProgress(self, self._decimate,
                                            'Decimating mesh')
+        self.add_vtk_error_handler(self._decimate)
                                            
         # now setup some defaults before our sync
-        self._config.targetReduction = self._decimate.GetTargetReduction()
+        self._config.target_reduction = self._decimate.GetTargetReduction() \
+                                        * 100.0
 
-        self._viewFrame = None
-        self._createViewFrame()
+        config_list = [
+            ('Target reduction (%):', 'target_reduction', 'base:float', 'text',
+             'Decimate algorithm will attempt to reduce by this much.')]
+        scriptedConfigModuleMixin.__init__(self, config_list)
 
-        # transfer these defaults to the logic
+        self._viewFrame = self._createWindow(
+            {'Module (self)' : self,
+             'vtkDecimatePro' : self._decimate,
+             'vtkTriangleFilter' : self._triFilter})
+
+        # pass the data down to the underlying logic
         self.configToLogic()
-
-        # then make sure they come all the way back up via self._config
+        # and all the way up from logic -> config -> view to make sure
         self.logicToConfig()
         self.configToView()
         
@@ -44,10 +54,12 @@ class decimate(moduleBase, vtkPipelineConfigModuleMixin):
         # we play it safe... (the graph_editor/module_manager should have
         # disconnected us by now)
         self.setInput(0, None)
-        # don't forget to call the close() method of the vtkPipeline mixin
-        vtkPipelineConfigModuleMixin.close(self)
-        # take out our view interface
-        self._viewFrame.Destroy()
+
+        # this will take care of all display thingies
+        scriptedConfigModuleMixin.close(self)
+
+        moduleBase.close(self)
+
         # get rid of our reference
         del self._decimate
         del self._triFilter
@@ -65,45 +77,17 @@ class decimate(moduleBase, vtkPipelineConfigModuleMixin):
         return self._decimate.GetOutput()
 
     def logicToConfig(self):
-        self._config.targetReduction = self._decimate.GetTargetReduction()
+        self._config.target_reduction = self._decimate.GetTargetReduction() \
+                                        * 100.0
 
     def configToLogic(self):
-        self._decimate.SetTargetReduction(self._config.targetReduction)
-
-    def viewToConfig(self):
-        f = genUtils.textToFloat(self._viewFrame.targetReductionText.\
-                                 GetValue(), self._config.targetReduction)
-        self._config.targetReduction = f / 100.0
-
-    def configToView(self):
-        self._viewFrame.targetReductionText.SetValue(
-            '%.2f' % (self._config.targetReduction * 100.0,))
+        self._decimate.SetTargetReduction(
+            self._config.target_reduction / 100.0)
 
     def executeModule(self):
         # get the filter doing its thing
+        self._triFilter.Update()
+        self.check_vtk_error()
         self._decimate.Update()
+        self.check_vtk_error()
 
-    def view(self, parent_window=None):
-        # if the window was visible already. just raise it
-        if not self._viewFrame.Show(True):
-            self._viewFrame.Raise()
-
-    def _createViewFrame(self):
-
-        # import the viewFrame (created with wxGlade)
-        import modules.Filters.resources.python.decimateFLTViewFrame
-        reload(modules.Filters.resources.python.decimateFLTViewFrame)
-
-        self._viewFrame = moduleUtils.instantiateModuleViewFrame(
-            self, self._moduleManager,
-            modules.Filters.resources.python.decimateFLTViewFrame.\
-            decimateFLTViewFrame)
-
-        objectDict = {'triangle filter' : self._triFilter,
-                      'decimator' : self._decimate}
-        moduleUtils.createStandardObjectAndPipelineIntrospection(
-            self, self._viewFrame, self._viewFrame.viewFramePanel,
-            objectDict, None)
-
-        moduleUtils.createECASButtons(self, self._viewFrame,
-                                      self._viewFrame.viewFramePanel)
