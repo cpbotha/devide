715e9488a6d8900f80813b94330f41734926745d
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-03-09 15:44:32 +0000

    Invoking script now works with both styles of pyInstaller: with and without support dir.

--- installer/devideInvokingScript.sh
+++ installer/devideInvokingScript.sh
@@ -2,12 +2,12 @@
 # Script that is used on *ix installations of devide to invoke the McMillan
 # built binary.  This sets up the LD_LIBRARY_PATH correctly, PYTHONPATH is
 # already correct.
-# $Id: devideInvokingScript.sh,v 1.2 2004/06/21 16:30:25 cpbotha Exp $
+# $Id$
 
 START_DIR=`pwd`
 cd `dirname $0`
 BINDIR=`pwd`
-export LD_LIBRARY_PATH=$BINDIR/support:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$BINDIR:$BINDIR/support:$LD_LIBRARY_PATH
 $BINDIR/devide.bin $*
 cd $START_DIR
 
