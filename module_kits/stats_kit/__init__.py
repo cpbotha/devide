64ea4f5f7698ffa1457094ef79f3015af0d965a2
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-03-27 14:39:36 +0000

    Refinements to sys.modules handling in stats_kit.

--- module_kits/stats_kit/__init__.py
+++ module_kits/stats_kit/__init__.py
@@ -11,6 +11,8 @@ Inserts the following modules in sys.modules: stats.
 @author: Charl P. Botha <http://cpbotha.net/>
 """
 
+import sys
+
 # you have to define this
 VERSION = 'Strangman - May 10, 2002'
 
@@ -18,5 +20,11 @@ def init(theModuleManager):
     # import the main module itself
     global stats
     import stats
+    # if we don't do this, the module will be in sys.modules as
+    # module_kits.stats_kit.stats because it's not in the sys.path.
+    # iow. if a module is in sys.path, "import module" will put 'module' in
+    # sys.modules.  if a module isn't, "import module" will put
+    # 'relative.path.to.module' in sys.path.
+    sys.modules['stats'] = stats
 
     theModuleManager.setProgress(100, 'Initialising stats_kit: complete.')
