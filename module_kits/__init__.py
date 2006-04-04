b331e2851b43cf560ab4571a7d41dd7384ffd095
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-04-04 20:43:10 +0000

    * Added dependency dictionary. * Added numpy_kit.

--- module_kits/__init__.py
+++ module_kits/__init__.py
@@ -6,7 +6,10 @@
 @ivar module_kits_list: All moduleKits in this list will have their init()s
 called with the moduleManager as parameter (after being imported).  Before
 this happens though, members of this list that are also in the no-kits list
-(defined by defaults.py or command-line) will be removed.
+(defined by defaults.py or command-line) will be removed.  The kits will be
+imported in the order that they are specified.  Make sure that kits dependent
+on other kits occur after those kits in the list, the dependency checker is
+that simple.
 
 @ivar crucial_kits: Usually, when a kit raises an exception during its init()
 call, this is ignored and DeVIDE starts up as usual.  However, if the module
@@ -14,7 +17,9 @@ is in the crucial_kit_list, DeVIDE will refuse to start up.
 """
 
 module_kit_list = ['wx_kit', 'vtk_kit', 'vtktud_kit',
-                   'itk_kit', 'matplotlib_kit', 'stats_kit']
+                   'itk_kit', 'numpy_kit', 'matplotlib_kit', 'stats_kit']
+
+dependencies_dict = {'matplotlib_kit' : ['numpy_kit']}
 
 crucial_kit_list = ['wx_kit', 'vtk_kit']
 
