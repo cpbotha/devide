b89631339cc2b7406b7ab35c948b3cbe189a0a96
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-04-12 16:13:54 +0000

    Initial import.

---
+++ module_kits/wx_kit/dvshell.py
@@ -0,0 +1,29 @@
+import wx
+from wx import py
+
+class DVShell(py.shell.Shell):
+    """DeVIDE shell.
+
+    Once again, PyCrust makes some pretty bad calls here and there.  With this
+    override we fix some of them.
+
+    1. passing locals=None will result in shell.Shell setting locals to
+    __main__.__dict__ (!!) in contrast to the default behaviour of the Python
+    code.InteractiveInterpreter , which is what we do here.
+    """
+
+    def __init__(self, parent, id=-1, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, style=wx.CLIP_CHILDREN,
+                 introText='', locals=None, InterpClass=None,
+                 startupScript=None, execStartupScript=True,
+                 *args, **kwds):
+
+        # default behaviour for InteractiveInterpreter
+        if locals is None:
+            locals = {"__name__": "__console__", "__doc__": None}
+
+        py.shell.Shell.__init__(self, parent, id, pos,
+                                size, style,
+                                introText, locals, InterpClass,
+                                startupScript, execStartupScript,
+                                *args, **kwds)
