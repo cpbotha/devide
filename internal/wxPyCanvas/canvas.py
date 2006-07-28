8d85cc03a651d84a272a02ba50d1f724525bea89
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-07-28 12:30:08 +0000

    Mouse capture activated for canvas.

--- internal/wxPyCanvas/canvas.py
+++ internal/wxPyCanvas/canvas.py
@@ -68,6 +68,15 @@ class canvas(wx.wxScrolledWindow, canvasSubject):
         return dc
 
     def OnMouseEvent(self, event):
+
+        # this seems to work fine under windows.  If we don't do this, and the
+        # mouse leaves the canvas, the rubber band remains and no selection
+        # is made.
+        if event.ButtonDown():
+            self.CaptureMouse()
+        elif event.ButtonUp():
+            self.ReleaseMouse()
+        
         # these coordinates are relative to the visible part of the canvas
         ex, ey = event.GetX(), event.GetY()
 
