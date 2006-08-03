b8a48b48f458076fe25a39a0722d8bc6d8d73365
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-08-03 13:02:16 +0000

    Added matplotlib to snippets directory.

---
+++ snippets/matplotlib_demo1.py
@@ -0,0 +1,21 @@
+# short DeVIDE matplotlib demo.
+
+from pylab import *
+
+# close previous figure if it exists
+try:
+    obj.mpl_close_figure(numpy_test_figure)
+except NameError:
+    pass
+
+numpy_test_figure = obj.mpl_new_figure()
+
+a = arange(-30, 30, 0.01)
+plot(a, sin(a) / a, label='sinc(x)')
+plot(a, cos(a), label='cos(x)')
+legend()
+grid()
+
+xlabel('x')
+ylabel('f(x)')
+
