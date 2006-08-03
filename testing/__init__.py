368faf62320aba9ab2933531ebcd77fb4b378555
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-08-03 15:11:40 +0000

    * Integrated new matplotlib test. * Added infrastructure to do image comparison tests with test images in images directory.

--- testing/__init__.py
+++ testing/__init__.py
@@ -9,8 +9,9 @@ import unittest
 from testing import basic_vtk
 from testing import basic_wx
 from testing import graph_editor
+from testing import matplotlib_tests
 
-module_list = [basic_vtk, basic_wx, graph_editor]
+module_list = [basic_vtk, basic_wx, graph_editor, matplotlib_tests]
 for m in module_list:
     reload(m)
 
@@ -23,11 +24,8 @@ class DeVIDETesting:
         
         suite_list = [basic_vtk.get_suite(self),
                       basic_wx.get_suite(self),
-                      graph_editor.get_suite(self)]
-
-        # do check for presence of itk_kit
-        #if _devideApp.mainConfig.useInsight:
-        #    suiteList.append(self.itkSuite)
+                      graph_editor.get_suite(self),
+                      matplotlib_tests.get_suite(self)]
 
         self.main_suite = unittest.TestSuite(tuple(suite_list))
         
@@ -36,8 +34,51 @@ class DeVIDETesting:
         runner.run(self.main_suite)
 
     def runSomeTest(self):
-        some_suite = graph_editor.get_some_suite(self)
+        some_suite = matplotlib_tests.get_suite(self)
 
         runner = unittest.TextTestRunner()
         runner.run(some_suite)
 
+    def get_images_dir(self):
+        """Return full path of directory with test images.
+        """
+
+        return os.path.join(os.path.dirname(__file__), 'images')
+        
+    def compare_png_images(self, image1_filename, image2_filename,
+                           threshold=16):
+
+        """Compare two PNG images on disc.  No two pixels may differ with more
+        than the default threshold.
+        """
+
+        import vtk
+
+        r1 = vtk.vtkPNGReader()
+        r1.SetFileName(image1_filename)
+        r1.Update()
+        # sometimes PNG files have an ALPHA component we have to chuck away
+        ec1 = vtk.vtkImageExtractComponents()
+        ec1.SetComponents(0,1,2)
+        ec1.SetInput(r1.GetOutput())
+        
+        r2 = vtk.vtkPNGReader()
+        r2.SetFileName(image2_filename)
+        r2.Update()
+        # sometimes PNG files have an ALPHA component we have to chuck away
+        ec2 = vtk.vtkImageExtractComponents()
+        ec2.SetComponents(0,1,2)
+        ec2.SetInput(r2.GetOutput())
+
+        idiff = vtk.vtkImageDifference()
+        idiff.SetThreshold(threshold)
+
+        idiff.SetImage(ec1.GetOutput())
+        idiff.SetInputConnection(ec2.GetOutputPort())
+
+        idiff.Update()
+
+        return idiff.GetThresholdedError()
+        
+
+        
---
+++ testing/matplotlib_tests.py
@@ -0,0 +1,65 @@
+"""Module to test basic matplotlib functionality.
+"""
+
+import os
+import unittest
+import tempfile
+
+class MPLTest(unittest.TestCase):
+    def test_figure_output(self):
+        """Test if matplotlib figure can be generated and wrote to disc.
+        """
+
+        # make sure the pythonshell is running
+        self._devide_app.get_interface()._handler_menu_python_shell(None)
+
+        # create new figure
+        python_shell = self._devide_app.get_interface()._python_shell
+        f = python_shell.mpl_new_figure()
+        import pylab
+
+
+        from pylab import arange, plot, sin, cos, legend, grid, xlabel, ylabel
+        a = arange(-30, 30, 0.01)
+        plot(a, sin(a) / a, label='sinc(x)')
+        plot(a, cos(a), label='cos(x)')
+        legend()
+        grid()
+        xlabel('x')
+        ylabel('f(x)')
+
+        # and save it to disc
+        filename1 = tempfile.mktemp(suffix='.png', prefix='tmp', dir=None)
+        f.savefig(filename1)
+
+        # get rid of the figure
+        python_shell.mpl_close_figure(f)
+
+        # now compare the bugger
+        test_fn = os.path.join(self._devide_testing.get_images_dir(),
+                               'mpl_test_figure_output.png')
+
+        err = self._devide_testing.compare_png_images(test_fn, filename1)
+
+        self.failUnless(err == 0, '%s differs from %s.' %
+                        (filename1, test_fn))
+
+
+def get_suite(devide_testing):
+    devide_app = devide_testing.devide_app
+    
+    # both of these tests require wx
+    mm = devide_app.get_module_manager()
+
+    mpl_suite = unittest.TestSuite()
+
+    if 'matplotlib_kit' in mm.module_kits.module_kit_list:
+        t = MPLTest('test_figure_output')
+        t._devide_app = devide_app
+        t._devide_testing = devide_testing
+        mpl_suite.addTest(t)
+
+    return mpl_suite
+
+    
+    
