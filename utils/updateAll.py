27ab39944280b7b7ee07bbdf8a2d8ac5156bcde5
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-08-05 16:37:20 +0000

    Update script now also takes care of itktud.

--- utils/updateAll.py
+++ utils/updateAll.py
@@ -1,7 +1,6 @@
 # updates all DeVIDE dependencies except ITK and VTK
-# i.e. do a cvs update, a cmake and a build of:
-# vtkdevide, vtktud
-# TODO: ConnectVTKITK
+# i.e. do a svn update, a cmake and a build of:
+# vtkdevide, vtktud, itktud (build and install)
 # $Id$
 # authored by Charl P. Botha http://cpbotha.net/
 
@@ -17,6 +16,8 @@ import sys
 # vtkdevideBinary = '/home/cpbotha/work/code/vtkdevide'
 # vtktudSource = '/home/cpbotha/work/code/vtktud'
 # vtktudBinary = '/home/cpbotha/work/code/vtktud'
+# itktudSource = 'c:/work/code/itktud'
+# itktudBinary = 'c:/build/itktud'
 # --- END updateAllDefaults
 try:
     import updateAllDefaults as defaults
@@ -27,12 +28,15 @@ except ImportError, e:
 # buildcommands for posix and MSVS 7.1 on Windows
 if os.name == 'posix':
     buildCommand = 'make'
+    install_command = 'make install'
     svn_update_command = 'svn update'
     cmake_update_command = 'cmake .'
 else:
     # first %s is to be replaced by the .sln file
     buildCommand = 'devenv %s /project ALL_BUILD /projectconfig ' \
                    '"RelWithDebInfo|Win32" /build RelWithDebInfo'
+    install_command = 'devenv %s /project INSTALL /projectconfig ' \
+                      '"RelWithDebInfo|Win32" /build RelWithDebInfo'
     svn_update_command = 'c:/apps/subversion/bin/svn update'
     cmake_update_command = '"c:\\program files\\cmake 2.4\\bin\\cmake" .'
 
@@ -54,39 +58,50 @@ def standardBuild(binary, slnFile):
     print 'Building with "%s"\n' % (theBuildCommand,)
 
     os.system(theBuildCommand)
-    
 
-def doUpdates(itkRequested):
+def standard_install(binary, sln_file):
+    print '\nInstalling from %s' % (binary,)
+    os.chdir(binary)
+
+    if os.name == 'posix':
+        the_install_command = install_command
+    else:
+        the_install_command = install_command % (sln_file,)
+
+    print 'Installing with "%s"\n' % (the_install_command,)
+
+    os.system(the_install_command)
+
+def doUpdates():
     standardUpdate(defaults.devideTopLevel)
     standardUpdate(defaults.vtkdevideSource)
     standardUpdate(defaults.vtktudSource)
-    if itkRequested:
-        #standardUpdate(defaults.wrapitkSource)
-        pass
+    standardUpdate(defaults.itktudSource)
 
-def doBuilds(itkRequested):
+def doBuilds():
     standardBuild(defaults.vtkdevideBinary, 'vtkdevide.sln')
     standardBuild(defaults.vtktudBinary, 'vtktud.sln')
-    if itkRequested:
-        #standardBuild(defaults.wrapitkBinary, 'wrapitk.sln')
-        pass
+    standardBuild(defaults.itktudBinary, 'itktud.sln')
+
+def do_installs():
+    standard_install(defaults.itktudBinary, 'itktud.sln')
 
 def dispUsage():
     print "-h or --help               : Display this message."
     print "--noUpdates                : Do not do any SVN updates."
     print "--noBuilds                 : Do not do any builds."
-    print "--noItk or --no-itk        : Do not build any itk-related source."
+    print "--no_installs              : Do not do any installs."
 
 def main():
     updatesRequested = True
     buildsRequested = True
-    itkRequested = True
+    installs_requested = True
     
     try:
         # 'p:' means -p with something after
         optlist, args = getopt.getopt(
             sys.argv[1:], 'h',
-            ['help', 'noUpdates', 'noBuilds', 'noItk', 'no-itk'])
+            ['help', 'noUpdates', 'noBuilds', 'no_installs'])
 
     except getopt.GetoptError,e:
         dispUsage()
@@ -103,14 +118,18 @@ def main():
         elif o in ('--noBuilds',):
             buildsRequested = False
 
-        elif o in ('--noItk', '--no-itk'):
-            itkRequested = False
+        elif o in ('--no_installs',):
+            installs_requested = False
+            
 
     if updatesRequested:
-        doUpdates(itkRequested)
+        doUpdates()
 
     if buildsRequested:
-        doBuilds(itkRequested)
+        doBuilds()
+
+    if installs_requested:
+        do_installs()
 
 if __name__ == '__main__':
     main()
