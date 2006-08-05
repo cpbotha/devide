282381a730cacfb84dfd362657396b193cd6c95c
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-08-05 22:07:15 +0000

    Sanitised line-endings using DeVIDE built-in editor.  Yay!

--- snippets/matplotlib_demo1.py
+++ snippets/matplotlib_demo1.py
@@ -1,7 +1,7 @@
-# short DeVIDE matplotlib demo.
-
+# short DeVIDE matplotlib demo.
+
 from pylab import *
-
+
 # close previous figure if it exists
 try:
     obj.mpl_close_figure(numpy_test_figure)
@@ -11,8 +11,8 @@ except NameError:
 numpy_test_figure = obj.mpl_new_figure()
 
 a = arange(-30, 30, 0.01)
-plot(a, sin(a) / a, label='sinc(x)')
-plot(a, cos(a), label='cos(x)')
+plot(a, sin(a) / a, label='sinc(x)')
+plot(a, cos(a), label='cos(x)')
 legend()
 grid()
 
