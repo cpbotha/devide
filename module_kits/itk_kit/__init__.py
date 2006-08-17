c4a4d6d9b363df009068df273ba451886af50c3a
Charl P. Botha <cpbotha@users.noreply.github.com>
2006-08-17 15:14:28 +0000

    Upped WrapITK version.

--- module_kits/itk_kit/__init__.py
+++ module_kits/itk_kit/__init__.py
@@ -12,7 +12,7 @@ import re
 import sys
 
 VERSION = ''
-ITK_VERSION_EXTRA = '-r ITK-2-8 (WrapITK 6.7.28)'
+ITK_VERSION_EXTRA = '-r ITK-2-8 (WrapITK 6.8.17)'
 
 def preImportITK(progressMethod):
     itkImportList = [('VXLNumericsPython', 'VXL Numerics'),
