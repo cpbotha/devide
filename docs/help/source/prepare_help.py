3a2e5d7f47fa783a7885e77b60c6d92ba9071a2d
Charl P. Botha <cpbotha@users.noreply.github.com>
2007-09-07 22:40:05 +0000

    Created script to do post-processing of HelpMaker generated files, leaving both htb and chm files in the right place.

---
+++ docs/help/source/prepare_help.py
@@ -0,0 +1,33 @@
+# python rules.  severely.
+# (c) 2007 cpbotha
+
+import glob
+import sys
+import os
+import shutil
+import zipfile
+
+def main():
+    cwd = os.path.abspath(os.curdir)
+    hhp_dir = os.path.join(cwd, 'devidehelp_tmphhp')
+
+    os.chdir(hhp_dir)
+
+    htb_list = glob.glob('*.html') +  \
+        glob.glob('*.png') + \
+        glob.glob('devide.hh?') + \
+        ['CSHelp.txt']
+
+    zf = zipfile.ZipFile('../../DeVIDE.htb', 'w', zipfile.ZIP_DEFLATED)
+    for fn in htb_list:
+        zf.write(fn)
+
+    zf.close()
+
+    # also copy the CHM file for the windows people.
+    shutil.copy('DeVIDE.chm', '../../')
+
+
+if __name__ == '__main__':
+    main()
+
