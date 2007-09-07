340a91a98d45da55e49a91b51ff324bc13a52850
Charl P. Botha <cpbotha@users.noreply.github.com>
2007-09-07 15:41:32 +0000

    Elements are now added to the BEGINNING of sys.path, and not to the end.

--- module_kits/numpy_kit/__init__.py
+++ module_kits/numpy_kit/__init__.py
@@ -24,10 +24,14 @@ def init(theModuleManager):
 
     # we add the path of this kit and of its numpy subdir so that
     # the frozen/installed version can import it
+    # with the new pyinstaller 1.3, it IMPORTANT to add this to the
+    # beginning of the sys.path, as on systems where Python is
+    # installed, this somehow also gets added to the sys.path and then
+    # parts of the system numpy are important!!
     p1 = os.path.dirname(__file__)
     p2 = os.path.join(p1, 'numpy')
-    sys.path.append(p1)
-    sys.path.append(p2)
+    sys.path.insert(0, p1)
+    sys.path.insert(0, p2)
 
     # after this, 'numpy' exists within our namespace, and
     # sys.modules['numpy'] contains a ref to our import (although it might
@@ -42,8 +46,9 @@ def init(theModuleManager):
 
     # remove the two paths that we inserted so we don't confuse anybody
     # with relative package imports that act strangely
-    del sys.path[-1]
-    del sys.path[-1]
+    # remove from beginning of path
+    del sys.path[0]
+    del sys.path[0]
 
     theModuleManager.setProgress(95, 'Initialising numpy_kit: import done')
 
