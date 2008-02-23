2eee4eebabff70639039cdae38855781ddbabdeb
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-02-23 13:20:41 +0000

    Much progress: we now get all the way to a complete rebased distdevide on Windows.

--- installer/make_dist.py
+++ installer/make_dist.py
@@ -8,7 +8,28 @@ import re
 import shutil
 import sys
 
+###################################################################
+# the following programmes should either be on your path, or you
+# should specify the full paths here.
+# Microsoft utility to rebase files.
+REBASE = "rebase"
+
+# end of programmes ###############################################
+
 PPF = "[*** DeVIDE make_dist ***]"
+S_PPF = "%s =====>>>" % (PPF,) # used for stage headers
+
+S_CLEAN_PYI = 'clean_pyi'
+S_RUN_PYI = 'run_pyi'
+S_WRAPITK_TREE = 'wrapitk_tree'
+S_REBASE_DLLS = 'rebase_dlls'
+S_PACKAGE_DIST = 'package_dist'
+DEFAULT_STAGES = '%s, %s, %s, %s, %s' % \
+        (S_CLEAN_PYI, S_RUN_PYI, S_WRAPITK_TREE, 
+                S_REBASE_DLLS, S_PACKAGE_DIST)
+
+
+
 
 
 ####################################################################
@@ -112,15 +133,23 @@ def usage():
     message = """
 make_dist.py - build DeVIDE distributables.
 
+
 Invoke as follows:
+
 python make_dist.py -s specfile -i installer_script
+
 where specfile is the pyinstaller spec file and installer_script
 refers to the full path of the pyinstaller Build.py 
 
 The specfile should be in the directory devide/installer, where devide
-contains the devide source that you are using to build the
-distributables.
-"""
+is the directory containing the devide source that you are using to
+build the distributables.
+
+Other switches:
+--stages : by default all stages are run.  With this parameter, a
+           subset of the stages can be specified.  The full list is: 
+           %s
+""" % (DEFAULT_STAGES,)
 
     print message
 
@@ -129,6 +158,8 @@ def clean_pyinstaller(md_paths):
     to do everything from scratch.  We usually do this before building
     full release versions.
     """
+
+    print S_PPF, "clean_pyinstaller"
     
     if os.path.isdir(md_paths.pyi_dist_dir):
         print PPF, "Removing distdevide..."
@@ -142,6 +173,8 @@ def run_pyinstaller(md_paths):
     """Run pyinstaller with the given parameters.  This does not clean
     out the dist and build directories before it begins
     """
+
+    print S_PPF, "run_pyinstaller"
 
     # old makePackage.sh would remove all *.pyc, *~ and #*# files
     cmd = '%s %s %s' % (sys.executable, md_paths.pyinstaller_script,
@@ -197,12 +230,20 @@ def package_dist(md_paths):
     posix)
     """
 
+    print S_PPF, "package_dist"
+
+def rebase_dlls(md_paths):
+    """Rebase all DLLs in the distdevide tree on Windows.
+    """
+
     if os.name == 'nt':
-        print PPF, 'Rebasing DLL / PYD files'
+        print S_PPF, "rebase_dlls"
         # get list of pyd / dll files, add newline to each and every
         # filename
         so_files = ['%s\n' % (i,) for i in
                 find_files(md_paths.pyi_dist_dir)]
+
+        print "Found %d DLL PYD files..." % (len(so_files),)
 
         # open file in specfile_dir, write the whole list
         dll_list_fn = os.path.join(
@@ -211,6 +252,28 @@ def package_dist(md_paths):
         dll_list.writelines(so_files)
         dll_list.close()
 
+        # now run rebase on the list
+        os.chdir(md_paths.specfile_dir)
+        ret = os.system(
+                '%s -b 0x60000000 -e 0x1000000 @dll_list.txt -v' %
+                (REBASE,))
+
+        # rebase returns 99 after rebasing, no idea why.
+        if ret != 99:
+            raise RuntimeError('Could not rebase DLLs.')
+
+def wrapitk_tree(md_paths):
+    print S_PPF, "wrapitk_tree"
+
+    py_file = os.path.join(md_paths.specfile_dir, 'wrapitk_tree.py')
+    cmd = "%s %s %s" % (sys.executable, py_file, md_paths.pyi_dist_dir) 
+    ret = os.system(cmd)
+
+    if ret != 0:
+        raise RuntimeError(
+        'Error creating self-contained WrapITK tree.')
+
+
 def windows_prereq_check():
     print PPF, 'WINDOWS prereq check'
 
@@ -218,7 +281,8 @@ def windows_prereq_check():
     # exits with return code 99 and outputs its stuff to stderr
     # with -b it exits with return code 0 (expected) and uses stdout
     v = find_command_with_ver(
-            'Microsoft Rebase (rebase.exe)', 'rebase -b 0x60000000',
+            'Microsoft Rebase (rebase.exe)', 
+            '%s -b 0x60000000' % (REBASE,),
             '^(REBASE):\s+Total.*$')
 
     return v
@@ -228,7 +292,7 @@ def main():
     try:
         optlist, args = getopt.getopt(
                 sys.argv[1:], 'hs:i:',
-                ['help', 'spec=','pyinstaller-script='])
+                ['help', 'spec=','pyinstaller-script=','stages='])
 
     except getopt.GetoptError,e:
         usage
@@ -236,6 +300,8 @@ def main():
 
     spec = None
     pyi_script = None
+    stages = DEFAULT_STAGES
+    
 
     for o, a in optlist:
         if o in ('-h', '--help'):
@@ -247,6 +313,10 @@ def main():
 
         elif o in ('-i', '--pyinstaller-script'):
             pyi_script = a
+
+        elif o in ('--stages'):
+            stages = a
+            
 
     if spec is None or pyi_script is None:
         # we need BOTH the specfile and pyinstaller script
@@ -262,10 +332,18 @@ def main():
 
     md_paths = MDPaths(spec, pyi_script)
 
-    #clean_pyinstaller(md_paths)
-    #run_pyinstaller(md_paths)
+    stages = [i.strip() for i in stages.split(',')]
 
-    package_dist(md_paths)    
+    if S_CLEAN_PYI in stages:
+        clean_pyinstaller(md_paths)
+    if S_RUN_PYI in stages:
+        run_pyinstaller(md_paths)
+    if S_WRAPITK_TREE in stages:
+        wrapitk_tree(md_paths)
+    if S_REBASE_DLLS in stages:
+        rebase_dlls(md_paths)
+    if S_PACKAGE_DIST in stages:
+        package_dist(md_paths)    
 
 
 
