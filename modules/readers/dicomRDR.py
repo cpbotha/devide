9b124bce155cc4194b3264568ed053fb2cc626c2
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-03-21 11:35:57 +0000

    Updated dicomRDR to use new misc_utils.

--- modules/readers/dicomRDR.py
+++ modules/readers/dicomRDR.py
@@ -1,6 +1,7 @@
 # $Id$
 
 import genUtils
+import misc_utils # new-style genUtils (in a module_kit)
 import os
 from moduleBase import moduleBase
 from moduleMixins import \
@@ -186,47 +187,6 @@ class dicomRDR(moduleBase,
         self._viewFrame.estimateSliceThicknessCheckBox.SetValue(
             self._config.estimateSliceThickness)
 
-    def _major_axis_from_iop_cosine(self, iop_cosine):
-        """Given an IOP direction cosine, i.e. either the row or column,
-        return a tuple with two components from LRPAFH signifying the
-        direction of the cosine.  For example, if the cosine is pointing from
-        Left to Right (1\0\0) we return ('L', 'R').
-
-        If the direction cosine is NOT aligned with any of the major axes,
-        we return None.
-
-        Based on a routine from dclunie's pixelmed software and info from
-        http://www.itk.org/pipermail/insight-users/2003-September/004762.html
-        but we've flipped some things around to make more sense.
-
-        IOP direction cosines are always in the RAH system:
-         * x is left to right
-         * y is posterior to anterior
-         * z is foot to head <-- seems this might be the other way around,
-           judging by dclunie's code.
-        """
-
-        obliquity_threshold = 0.8
-
-        orientation_x = [('L', 'R'), ('R', 'L')][int(iop_cosine[0] > 0)]
-        orientation_y = [('P', 'A'), ('A', 'P')][int(iop_cosine[1] > 0)]
-        orientation_z = [('H', 'F'), ('F', 'H')][int(iop_cosine[2] > 0)]
-
-        abs_x = abs(iop_cosine[0])
-        abs_y = abs(iop_cosine[1])
-        abs_z = abs(iop_cosine[2])
-
-        if abs_x > obliquity_threshold and abs_x > abs_y and abs_x > abs_z:
-            return orientation_x
-
-        elif abs_y > obliquity_threshold and abs_y > abs_x and abs_y > abs_z:
-            return orientation_y
-
-        elif abs_z > obliquity_threshold and abs_z > abs_x and abs_z > abs_y:
-            return orientation_z
-
-        else:
-            return None
     
     def execute_module(self):
         # get the vtkDICOMVolumeReader to try and execute
@@ -243,9 +203,9 @@ class dicomRDR(moduleBase,
         norm = [0,0,0]
         vtk.vtkMath.Cross(row, col, norm)
 
-        xl = self._major_axis_from_iop_cosine(row)
-        yl = self._major_axis_from_iop_cosine(col)
-        zl = self._major_axis_from_iop_cosine(norm)
+        xl = misc_utils.major_axis_from_iop_cosine(row)
+        yl = misc_utils.major_axis_from_iop_cosine(col)
+        zl = misc_utils.major_axis_from_iop_cosine(norm)
 
         lut = {'L' : 0, 'R' : 1, 'P' : 2, 'A' : 3, 'F' : 4, 'H' : 5}
 
