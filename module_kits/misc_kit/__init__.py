04d7c20d7782ef6b50192ce06e2649962067b17e
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-05-05 22:06:53 +0000

    misc_kit does not create 'misc_utils' in sys.modules anymore.

--- module_kits/misc_kit/__init__.py
+++ module_kits/misc_kit/__init__.py
@@ -9,8 +9,8 @@ VERSION = 'INTEGRATED'
 def init(module_manager):
     global misc_utils
     import misc_utils
-    # make sure misc_utils is available everywhere
-    sys.modules['misc_utils'] = misc_utils
+
+    global types
     module_manager.set_progress(100, 'Initialising misc_kit: complete.')
 
 
