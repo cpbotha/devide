bef05d2c0d7cbd484f99eabc807aa1330a6c26b9
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-06-11 12:44:53 +0000

    Removed unused updateCallsExecuteModule mixin.

--- gen_mixins.py
+++ gen_mixins.py
@@ -39,25 +39,3 @@ class SubjectMixin(object):
         self._observers[event_name].remove(observer)
 
 
-class updateCallsExecuteModuleMixin(object):
-    """The DeVIDE API requires that calling Update on outputData should
-    ensure that that data is current.  This mixin does that by calling
-    execute_module on the generating module when Update is invoked.
-    """
-
-    def __init__(self, d3Module):
-        self._d3Module = d3Module
-
-    def close(self):
-        # get rid of our reference
-        del self._d3Module
-
-    def Update(self):
-        """Part of our API.  If Update is called, we somehow have to make
-        sure that we are current.
-        """
-        if self._d3Module:
-            self._d3Module._moduleManager.execute_module(self._d3Module)
-
-
-
