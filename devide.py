7a54d362946bf7f8c7699615eddd320750c95fb2
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-06-12 12:21:04 +0000

    One more stamp, this could be a test version.

--- devide.py
+++ devide.py
@@ -10,7 +10,7 @@ import re
 SVN_REVISION_TEXT = "$Revision$"
 # updated stamp to have SVN_REVISION_TEXT updated.  This is mostly
 # only relevant for DEV builds.
-STAMP = "20080612-1330"
+STAMP = "20080612-1420"
 
 # the main release version: year.month (ubuntu-style) on release
 # branches (the branch is also named vYEAR.MONTH), DEV for trunk
