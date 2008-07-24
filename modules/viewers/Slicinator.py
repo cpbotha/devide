23ceffa408799fb3d5de60fa2a86419afd3508d8
Charl P. Botha <cpbotha@users.noreply.github.com>
2008-07-24 17:06:09 +0000

    Initial import of The Slicinator.  It will be back!

---
+++ modules/viewers/Slicinator.py
@@ -0,0 +1,15 @@
+# Copyright (c) Charl P. Botha, TU Delft
+# All rights reserved.
+# See COPYRIGHT for details.
+
+# general requirements:
+# * multiple objects (i.e. multiple coloured contours per slice)
+
+# slice segmentation modes:
+# * polygon mode
+# * freehand drawing
+# * 2d levelset
+
+class Slicinator:
+    pass
+
