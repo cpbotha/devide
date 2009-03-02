8b5186d720f580122357c3e83232ecd1bcc6f97c
Charl P. Botha <cpbotha@users.noreply.github.com>
2009-03-02 11:02:35 +0000

    Drastically improved landmarkTransform usage.  You can now add any mix of source and target landmarks to any mix of the first two inputs.  It will add all landmarks to the same list, then fish them out by name. The source landmarks are canonical in this regard.  Thanks to Martijn Steenwijk for the suggestion!

--- modules/filters/landmarkTransform.py
+++ modules/filters/landmarkTransform.py
@@ -1,4 +1,4 @@
-# landmarkTransform.py copyright (c) 2003 by Charl P. Botha <cpbotha@ieee.org>
+# landmark_transform.py copyright (c) 2003 by Charl P. Botha <cpbotha@ieee.org>
 # $Id$
 # see module documentation
 
@@ -13,9 +13,10 @@ class landmarkTransform(ScriptedConfigModuleMixin, ModuleBase):
     def __init__(self, module_manager):
         ModuleBase.__init__(self, module_manager)
 
-        self._inputPoints = None
-        self._sourceLandmarks = None
-        self._targetLandmarks = None
+        self._input_points = [None, None]
+
+        self._source_landmarks = None
+        self._target_landmarks = None
 
         self._config.mode = 'Rigid'
 
@@ -25,12 +26,12 @@ class landmarkTransform(ScriptedConfigModuleMixin, ModuleBase):
                        'Affine: rigid + scaling + shear',
                        ('Rigid', 'Similarity', 'Affine'))]
 
-        self._landmarkTransform = vtk.vtkLandmarkTransform()
+        self._landmark_transform = vtk.vtkLandmarkTransform()
 
         ScriptedConfigModuleMixin.__init__(
             self, configList,
             {'Module (self)' : self,
-             'vtkLandmarkTransform': self._landmarkTransform})
+             'vtkLandmarkTransform': self._landmark_transform})
 
         self.sync_module_logic_with_config()
 
@@ -44,21 +45,22 @@ class landmarkTransform(ScriptedConfigModuleMixin, ModuleBase):
         ScriptedConfigModuleMixin.close(self)
         
         # get rid of our reference
-        del self._landmarkTransform
+        del self._landmark_transform
 
     def get_input_descriptions(self):
-        return ('Source and Target Points',)
+        return ('Source and/or Target Points', 
+        'Source and/or Target Points')
 
-    def set_input(self, idx, inputStream):
-        if inputStream is not self._inputPoints:
+    def set_input(self, idx, input_stream):
+        if input_stream is not self._input_points[idx]:
 
-            if inputStream == None:
-                self._inputPoints = None
+            if input_stream == None:
+                self._input_points[idx] = None
 
-            elif hasattr(inputStream, 'devideType') and \
-                 inputStream.devideType == 'namedPoints':
+            elif hasattr(input_stream, 'devideType') and \
+                 input_stream.devideType == 'namedPoints':
 
-                self._inputPoints = inputStream
+                self._input_points[idx] = input_stream
 
             else:
                 raise TypeError, 'This input requires a named points type.'
@@ -67,65 +69,84 @@ class landmarkTransform(ScriptedConfigModuleMixin, ModuleBase):
         return ('vtkTransform',)
 
     def get_output(self, idx):
-            return self._landmarkTransform
+            return self._landmark_transform
 
     def logic_to_config(self):
-        mas = self._landmarkTransform.GetModeAsString()
+        mas = self._landmark_transform.GetModeAsString()
         if mas == 'RigidBody':
             mas = 'Rigid'
         self._config.mode = mas
     
     def config_to_logic(self):
         if self._config.mode == 'Rigid':
-            self._landmarkTransform.SetModeToRigidBody()
+            self._landmark_transform.SetModeToRigidBody()
         elif self._config.mode == 'Similarity':
-            self._landmarkTransform.SetModeToSimilarity()
+            self._landmark_transform.SetModeToSimilarity()
         else:
-            self._landmarkTransform.SetModeToAffine()
+            self._landmark_transform.SetModeToAffine()
     
     def execute_module(self):
         self._sync_with_input_points()
-        self._landmarkTransform.Update()
+        self._landmark_transform.Update()
 
     def _sync_with_input_points(self):
         # the points have changed, let's see if they really have
 
-        if not self._inputPoints:
+        if not (self._input_points[0] or self._input_points[1]):
             return
-        
-        tempSourceLandmarks = [i['world'] for i in self._inputPoints
-                               if i['name'].lower().startswith('source')]
-        tempTargetLandmarks = [i['world'] for i in self._inputPoints
-                               if i['name'].lower().startswith('target')]
 
-        print "hi there"
-        
-        if tempSourceLandmarks != self._sourceLandmarks or \
-           tempTargetLandmarks != self._targetLandmarks:
+        all_points = self._input_points[0] + self._input_points[1]
+       
 
-            print "seems like I have to update"
+        # we want i['world'] eventually
+        temp_source_points = [i for i in all_points
+                              if i['name'].lower().startswith('source')]
+        temp_target_points = [i for i in all_points
+                              if i['name'].lower().startswith('target')]
 
-            if len(tempSourceLandmarks) != len(tempTargetLandmarks):
-                raise Exception(
-                    "landmarkTransform: Your 'Source' landmark set and "
-                    "'Target' landmark set should be of equal size.")
+        # now put both source and target points in dict keyed on short
+        # name
+        slen = len('source')
+        sdict = {}
+        tdict = {}
+        for dict, points in [(sdict, temp_source_points), (tdict,
+            temp_target_points)]:
+            for point in points:
+                # turn 'source moo maa ' into 'moo maa'
+                name = point['name'][slen:].strip()
+                # stuff world position in dict keyed by name
+                dict[name] = point['world']
 
-            else:
-                self._sourceLandmarks = tempSourceLandmarks
-                self._targetLandmarks = tempTargetLandmarks
+        # use this as decorator list
+        snames = sdict.keys()
+        snames.sort()
 
-                sourceLandmarks = vtk.vtkPoints()
-                targetLandmarks = vtk.vtkPoints()
-                landmarkPairs = ((self._sourceLandmarks, sourceLandmarks),
-                                 (self._targetLandmarks, targetLandmarks))
-                
-                for lmp in landmarkPairs:
-                    lmp[1].SetNumberOfPoints(len(lmp[0]))
-                    for pointIdx in range(len(lmp[0])):
-                        lmp[1].SetPoint(pointIdx, lmp[0][pointIdx])
-                                 
-                self._landmarkTransform.SetSourceLandmarks(sourceLandmarks)
-                self._landmarkTransform.SetTargetLandmarks(targetLandmarks)
-                
+        temp_source_landmarks = [sdict[name] for name in snames]
+        try:
+            temp_target_landmarks = [tdict[name] for name in snames]
+        except KeyError:
+            raise RuntimeError("There is no target point named '%s'" %
+                    (name))
+
+
+        if temp_source_landmarks != self._source_landmarks or \
+           temp_target_landmarks != self._target_landmarks:
+
+            self._source_landmarks = temp_source_landmarks
+            self._target_landmarks = temp_target_landmarks
+
+            source_landmarks = vtk.vtkPoints()
+            target_landmarks = vtk.vtkPoints()
+            landmarkPairs = ((self._source_landmarks, source_landmarks),
+                             (self._target_landmarks, target_landmarks))
+            
+            for lmp in landmarkPairs:
+                lmp[1].SetNumberOfPoints(len(lmp[0]))
+                for pointIdx in range(len(lmp[0])):
+                    lmp[1].SetPoint(pointIdx, lmp[0][pointIdx])
+                             
+            self._landmark_transform.SetSourceLandmarks(source_landmarks)
+            self._landmark_transform.SetTargetLandmarks(target_landmarks)
+            
         
         
