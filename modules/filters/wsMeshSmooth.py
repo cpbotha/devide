a326f18738c5956ca5fc9ba76cc30152faadf270
Charl P. Botha <cpbotha@users.noreply.github.com>
2009-03-19 21:53:38 +0000

    Added non-manifold smoothing switch.

--- modules/filters/wsMeshSmooth.py
+++ modules/filters/wsMeshSmooth.py
@@ -20,6 +20,7 @@ class wsMeshSmooth(ScriptedConfigModuleMixin, ModuleBase):
         self._config.passBand = 0.1
         self._config.featureEdgeSmoothing = False
         self._config.boundarySmoothing = True
+        self._config.non_manifold_smoothing = False
 
         config_list = [
             ('Number of iterations', 'numberOfIterations', 'base:int', 'text',
@@ -30,7 +31,11 @@ class wsMeshSmooth(ScriptedConfigModuleMixin, ModuleBase):
             ('Feature edge smoothing', 'featureEdgeSmoothing', 'base:bool',
              'checkbox', 'Smooth feature edges (large dihedral angle)'),
             ('Boundary smoothing', 'boundarySmoothing', 'base:bool',
-             'checkbox', 'Smooth boundary edges (edges with only one face).')]
+             'checkbox', 'Smooth boundary edges (edges with only one face).'),
+            ('Non-manifold smoothing', 'non_manifold_smoothing',
+            'base:bool', 'checkbox',
+            'Smooth non-manifold vertices, for example output of '
+            'discrete marching cubes (multi-material).')]
 
         ScriptedConfigModuleMixin.__init__(
             self, config_list,
@@ -73,6 +78,8 @@ class wsMeshSmooth(ScriptedConfigModuleMixin, ModuleBase):
             self._wsPDFilter.GetFeatureEdgeSmoothing())
         self._config.boundarySmoothing = bool(
             self._wsPDFilter.GetBoundarySmoothing())
+        self._config.non_manifold_smoothing = bool(
+                self._wsPDFilter.GetNonManifoldSmoothing())
 
     def config_to_logic(self):
         self._wsPDFilter.SetNumberOfIterations(self._config.numberOfIterations)
@@ -81,6 +88,8 @@ class wsMeshSmooth(ScriptedConfigModuleMixin, ModuleBase):
             self._config.featureEdgeSmoothing)
         self._wsPDFilter.SetBoundarySmoothing(
             self._config.boundarySmoothing)
+        self._wsPDFilter.SetNonManifoldSmoothing(
+                self._config.non_manifold_smoothing)
 
 
     def execute_module(self):
