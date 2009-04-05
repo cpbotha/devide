235f6190c66437bca76d4c7e9709c3460a20625b
Charl P. Botha <cpbotha@users.noreply.github.com>
2009-04-05 13:14:51 +0000

    More notes added w.r.t. font tuning.

--- module_kits/wx_kit/utils.py
+++ module_kits/wx_kit/utils.py
@@ -5,7 +5,8 @@
 import wx
 
 def get_system_font_size():
-    #ft = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
+    # wx.SYS_ANSI_FIXED_FONT seems to return reliable settings under
+    # Windows and Linux.  SYS_SYSTEM_FONT doesn't do so well on Win.
     ft = wx.SystemSettings.GetFont(wx.SYS_ANSI_FIXED_FONT)
     return ft.GetPointSize()
 
@@ -16,6 +17,11 @@ def create_html_font_size_array():
     For system font size 8, this should be:
     [4,6,8,10,12,14,16]
     corresponding with HTML sizes -2 to +4
+
+    Currently used to set font sizes on the module documentation
+    window.  Would have liked to use on the module list box as well,
+    but we can't get to the underlying wxHTML to set it, so we're
+    forced to use font size=-1 in that case.
     """
 
     sfs = get_system_font_size()
