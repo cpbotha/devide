5c8a48f0131cdb6aa19c2bb72d397793d212de99
Charl P. Botha <cpbotha@users.noreply.github.com>
2009-07-30 07:21:04 +0000

    Timestamped so the dev version gets a slightly more up to date version.

--- devide.py
+++ devide.py
@@ -10,7 +10,7 @@ import re
 SVN_REVISION_TEXT = "$Revision$"
 # updated stamp to have SVN_REVISION_TEXT updated.  This is mostly
 # only relevant for DEV builds.
-STAMP = "20090202-1351"
+STAMP = "20090730-0920"
 
 # the main release version: year.month (ubuntu-style) on release
 # branches (the branch is also named vYEAR.MONTH), DEV for trunk
