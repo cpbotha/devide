6115b9e790e7c60e02f0d4feedb2668e97ea646b
Charl Botha <cpbotha@vxlabs.com>
2015-06-11 21:32:54 +0200

    Add conda recipe for vtkdevide

---
+++ conda-recipes/vtkdevide/bld.bat
@@ -0,0 +1,1 @@
+@echo No Windows support.
---
+++ conda-recipes/vtkdevide/run_test.py
@@ -0,0 +1,4 @@
+# this imports all of the modules belonging to Python
+import vtk
+# mkae sure that it's the right version!
+assert(vtk.vtkVersion().GetVTKVersion() == '6.2.0')
